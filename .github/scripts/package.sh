d2c7aa0bf6631053eebbcb0213cfd357a33b1766
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-01-08 00:48:56 +0100

    Workflow fixes for windows and linux

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -4,8 +4,16 @@ rm -f PlugData-$1.zip
 cp -r Plugins PlugData
 
 mkdir Plugins/LV2/PlugData.lv2
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+./Plugins/LV2/lv2_file_generator Plugins/LV2/PlugData_LV2.so PlugData
+cp Plugins/LV2/PlugData_LV2.so Plugins/LV2/PlugData.lv2/PlugData.so
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
 ./Plugins/LV2/lv2_file_generator Plugins/LV2/PlugData_LV2.dylib PlugData
 cp Plugins/LV2/PlugData_LV2.dylib Plugins/LV2/PlugData.lv2/PlugData.dylib
+fi
+
 mv manifest.ttl Plugins/LV2/PlugData.lv2/manifest.ttl
 mv presets.ttl Plugins/LV2/PlugData.lv2/presets.ttl
 mv PlugData.ttl Plugins/LV2/PlugData.lv2/PlugData.ttl
