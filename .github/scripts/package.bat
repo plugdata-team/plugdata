c6c761f10b780f8d5666a7e767d8da8190776018
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-01-08 00:21:54 +0100

    Hopefully fixed windows build automation

--- .github/scripts/package.bat
+++ .github/scripts/package.bat
@@ -1,17 +1,17 @@
 @echo off
-del PlugData-%1.zip /Q
-xcopy Plugins PlugData /E /I
+del PlugData-%1.zip \Q
+xcopy Plugins PlugData \E \I
 
-md Plugins/LV2/PlugData.lv2
-./Plugins/LV2/lv2_file_generator Plugins/LV2/PlugData_LV2.dylib PlugData
-copy Plugins/LV2/PlugData_LV2.dylib Plugins/LV2/PlugData.lv2/PlugData.dylib
-move manifest.ttl Plugins/LV2/PlugData.lv2/manifest.ttl
-move presets.ttl Plugins/LV2/PlugData.lv2/presets.ttl
-move PlugData.ttl Plugins/LV2/PlugData.lv2/PlugData.ttl
+md PlugData\LV2\PlugData.lv2
+PlugData\LV2\lv2_file_generator.exe PlugData\LV2\PlugData_LV2.dylib PlugData
+copy PlugData\LV2\PlugData_LV2.dylib PlugData\LV2\PlugData.lv2\PlugData.dylib
+move manifest.ttl PlugData\LV2\PlugData.lv2\manifest.ttl
+move presets.ttl PlugData\LV2\PlugData.lv2\presets.ttl
+move PlugData.ttl PlugData\LV2\PlugData.lv2\PlugData.ttl
 
-md Plugins/LV2/Extra
-move Plugins/LV2/lv2_file_generator Plugins/LV2/Extra/lv2_file_generator
-move Plugins/LV2/PlugData_LV2.dylib Plugins/LV2/Extra/PlugData.dylib
+md PlugData\LV2\Extra
+move PlugData\LV2\lv2_file_generator.exe PlugData\LV2\Extra\lv2_file_generator.exe
+move PlugData\LV2\PlugData_LV2.dylib PlugData\LV2\Extra\PlugData.dylib
 copy README.md PlugData\README.md
 copy ChangeLog.md PlugData\ChangeLog.md
 copy LICENSE PlugData\LICENSE.txt
