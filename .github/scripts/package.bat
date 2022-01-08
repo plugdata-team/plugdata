39a9d4ab704c9e7964fc84d8adf09993f086f65c
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-01-08 13:59:28 +0100

    Oops, should work now

--- .github/scripts/package.bat
+++ .github/scripts/package.bat
@@ -21,7 +21,7 @@ move PlugData\VST3\PlugData.vst3\Contents\x86-win\PlugData.vst3 PlugData\VST3\Pl
 rd PlugData\VST3\PlugData.vst3\ /S /Q
 move PlugData\VST3\PlugData_temp.vst3 PlugData\VST3\PlugData.vst3
 
-move PlugData\VST3\PlugDataFx.vst3\Contents\x86-win\PlugData.vst3 PlugData\VST3\PlugDataFx_temp.vst3
+move PlugData\VST3\PlugDataFx.vst3\Contents\x86-win\PlugDataFx.vst3 PlugData\VST3\PlugDataFx_temp.vst3
 rd PlugData\VST3\PlugDataFx.vst3\ /S /Q
 move PlugData\VST3\PlugDataFx_temp.vst3 PlugData\VST3\PlugDataFx.vst3
 
