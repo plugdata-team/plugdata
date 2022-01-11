f4e5fb2d9089dd05a097d9b6d7b0ef9e6b43351b
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-01-11 01:44:08 +0100

    Added gitignore, fixed LV2 build automation for windows

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -3,22 +3,22 @@
 rm -f PlugData-$1
 cp -r Plugins PlugData
 
-mkdir Plugins/LV2/PlugData.lv2
+mkdir PlugData/LV2/PlugData.lv2
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 ./Plugins/LV2/lv2_file_generator Plugins/LV2/PlugData_LV2.so PlugData
-mv Plugins/LV2/PlugData_LV2.so Plugins/LV2/PlugData.lv2/PlugData.so
+mv PlugData/LV2/PlugData_LV2.so Plugins/LV2/PlugData.lv2/PlugData.so
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-./Plugins/LV2/lv2_file_generator Plugins/LV2/PlugData_LV2.dylib PlugData
-mv Plugins/LV2/PlugData_LV2.dylib Plugins/LV2/PlugData.lv2/PlugData.dylib
+./Plugins/LV2/lv2_file_generator PlugData/LV2/PlugData_LV2.dylib PlugData
+mv PlugData/LV2/PlugData_LV2.dylib Plugins/LV2/PlugData.lv2/PlugData.dylib
 fi
 
-mv manifest.ttl Plugins/LV2/PlugData.lv2/manifest.ttl
-mv presets.ttl Plugins/LV2/PlugData.lv2/presets.ttl
-mv PlugData.ttl Plugins/LV2/PlugData.lv2/PlugData.ttl
+mv manifest.ttl PlugData/LV2/PlugData.lv2/manifest.ttl
+mv presets.ttl PlugData/LV2/PlugData.lv2/presets.ttl
+mv PlugData.ttl PlugData/LV2/PlugData.lv2/PlugData.ttl
 
-rm  Plugins/LV2/lv2_file_generator
+rm PlugData/LV2/lv2_file_generator
 
 cp README.md PlugData/README.md
 cp LICENSE PlugData/LICENSE.txt
