77facba22e7b4517f3ead3bca898ff9989e729ed
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-01-11 01:44:08 +0100

    Added gitignore, fixed LV2 build automation for windows

--- .github/scripts/package.bat
+++ .github/scripts/package.bat
@@ -4,7 +4,7 @@ xcopy Plugins PlugData /S /H /E /I
 
 md PlugData\LV2\PlugData.lv2
 start PlugData\LV2\lv2_file_generator.exe PlugData\LV2\PlugData_LV2.dll PlugData
-copy PlugData\LV2\PlugData_LV2.dll PlugData\LV2\PlugData.lv2\PlugData.dll
+move PlugData\LV2\PlugData_LV2.dll PlugData\LV2\PlugData.lv2\PlugData.dll
 move manifest.ttl PlugData\LV2\PlugData.lv2\manifest.ttl
 move presets.ttl PlugData\LV2\PlugData.lv2\presets.ttl
 move PlugData.ttl PlugData\LV2\PlugData.lv2\PlugData.ttl
