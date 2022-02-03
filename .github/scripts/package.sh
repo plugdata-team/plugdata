f45f623162b55a21de065e53288a98ecef1b2537
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-02-03 16:23:26 +0100

    Fixing build scripts to get correct LV2 output

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -6,12 +6,12 @@ cp -r Plugins PlugData
 mkdir PlugData/LV2/PlugData.lv2
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-./Plugins/LV2/lv2_file_generator Plugins/LV2/PlugData_LV2.so PlugData
-mv PlugData/LV2/PlugData_LV2.so Plugins/LV2/PlugData.lv2/PlugData.so
+./Plugins/LV2/lv2_file_generator PlugData/LV2/PlugData_LV2.so PlugData
+mv PlugData/LV2/PlugData_LV2.so PlugData/LV2/PlugData.lv2/PlugData.so
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 ./Plugins/LV2/lv2_file_generator PlugData/LV2/PlugData_LV2.dylib PlugData
-mv PlugData/LV2/PlugData_LV2.dylib Plugins/LV2/PlugData.lv2/PlugData.dylib
+mv PlugData/LV2/PlugData_LV2.dylib PlugData/LV2/PlugData.lv2/PlugData.dylib
 fi
 
 mv manifest.ttl PlugData/LV2/PlugData.lv2/manifest.ttl
