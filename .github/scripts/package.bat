d2a5cfda9eed1be4e165daab02ef7d9f037bc221
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-02-03 17:06:22 +0100

    Fixing windows LV2 archiving script

--- .github/scripts/package.bat
+++ .github/scripts/package.bat
@@ -2,12 +2,20 @@
 del PlugData-%1.zip /Q
 xcopy Plugins PlugData /S /H /E /I
 
-md PlugData\LV2\PlugData.lv2
-start PlugData\LV2\lv2_file_generator.exe PlugData\LV2\PlugData_LV2.dll PlugData
-move PlugData\LV2\PlugData_LV2.dll PlugData\LV2\PlugData.lv2\PlugData.dll
-xcopy manifest.ttl PlugData\LV2\PlugData.lv2\manifest.ttl
-xcopy presets.ttl PlugData\LV2\PlugData.lv2\presets.ttl
-xcopy PlugData.ttl PlugData\LV2\PlugData.lv2\PlugData.ttl
+
+cd PlugData\LV2
+md PlugData.lv2
+start lv2_file_generator.exe PlugData_LV2.dll PlugData
+
+move PlugData_LV2.dll PlugData.lv2\PlugData.dll
+xcopy manifest.ttl PlugData.lv2\manifest.ttl
+xcopy presets.ttl PlugData.lv2\presets.ttl
+xcopy PlugData.ttl PlugData.lv2\PlugData.ttl
+
+del lv2_file_generator.exe /F
+
+cd ..
+cd ..
 
 move PlugData\VST3\PlugData.vst3\Contents\x86-win\PlugData.vst3 PlugData\VST3\PlugData_temp.vst3
 move PlugData\VST3\PlugData.vst3\Contents\x86_64-win\PlugData.vst3 PlugData\VST3\PlugData_temp.vst3
@@ -20,7 +28,6 @@ rd PlugData\VST3\PlugDataFx.vst3\ /S /Q
 move PlugData\VST3\PlugDataFx_temp.vst3 PlugData\VST3\PlugDataFx.vst3
 
 
-del PlugData\LV2\lv2_file_generator.exe /F
 
 copy README.md PlugData\README.md
 copy ChangeLog.md PlugData\ChangeLog.md
