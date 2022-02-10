038b3168d9639c9fff2b5a4d8df18a1938c80845
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-02-10 14:35:34 +0100

    Fixed linker errors

--- Libraries/cyclone/shared/common/magicbit.c
+++ Libraries/cyclone/shared/common/magicbit.c
@@ -17,20 +17,6 @@ struct _outlet // local to m_obj.c.
 /* LATER export write access to o_connections field ('grab' class).
 LATER encapsulate 'traverseoutlet' routines (not in the stable API yet). */
 
-
-void magic_setnan(t_float *in) {
-	union magic_ui32_fl input_u;
-	input_u.uif_uint32 = CYCLONE_MAGIC_NAN;
-	*in = input_u.uif_float;
-}
-
-int magic_isnan(t_float in) {
-	union magic_ui32_fl input_u;
-	input_u.uif_float = in;
-	return (((input_u.uif_uint32 & 0x7f800000ul) == 0x7f800000ul) &&
-			(input_u.uif_uint32 & 0x007fffff));
-}
-
 int magic_isinf(t_float in) {
 	union magic_ui32_fl input_u;
 	input_u.uif_float = in;
@@ -48,17 +34,6 @@ t_outconnect *magic_outlet_nextconnection(t_outconnect *last, t_object **destp, 
     t_inlet *dummy;
     return (obj_nexttraverseoutlet(last, destp, &dummy, innop));
     }
-
-int magic_inlet_connection(t_object *x, t_glist *glist, int inno, t_symbol *outsym)
-{
-    t_linetraverser t;
-    linetraverser_start(&t, glist);
-    while (linetraverser_next(&t))
-        if (t.tr_ob2 == x && t.tr_inno == inno &&
-            (!outsym || outsym == outlet_getsymbol(t.tr_outlet)))
-        return (1);
-    return (0);
-}
 
 /*---------------------------------------*/
 
