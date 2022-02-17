4e7c6450fa9f4fdb2fada1a3448d1bb5be5b1cd9
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-02-17 03:33:11 +0100

    Added new statusbar class

---
+++ Source/PluginConfig.h
@@ -0,0 +1,19 @@
+/*
+ // Copyright (c) 2021-2022 Timothy Schoen
+ // For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ // WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+*/
+
+#pragma once
+
+#define JUCE_DISPLAY_SPLASH_SCREEN 0
+
+#define JucePlugin_LV2URI (juce::String("urn:PlugData:") + juce::String("PlugData")).toUTF8()
+
+namespace ProjectInfo
+{
+const char* const companyName = "Octagon";
+const char* const versionString = "0.3.2";
+}  // namespace ProjectInfo
+
+
---
+++ Source/Statusbar.h
@@ -0,0 +1,79 @@
+/*
+ // Copyright (c) 2021-2022 Timothy Schoen.
+ // For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ // WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+*/
+#pragma once
+#include <JuceHeader.h>
+
+#include <memory>
+
+
+struct LevelMeter;
+struct MidiBlinker;
+struct PlugDataAudioProcessor;
+
+struct StatusbarSource
+{
+    void processBlock(const AudioBuffer<float>& buffer, MidiBuffer& midiIn, MidiBuffer& midiOut);
+    
+    void prepareToPlay(int numChannels);
+    
+    std::atomic<bool> midiReceived;
+    std::atomic<bool> midiSent;
+    std::atomic<float> level[2];
+    
+    int numChannels;
+    
+    Time lastMidiIn;
+    Time lastMidiOut;
+    
+};
+
+struct Statusbar : public Component, public Timer, public KeyListener
+{
+    
+    PlugDataAudioProcessor& pd;
+    
+    Statusbar(PlugDataAudioProcessor& processor);
+    ~Statusbar();
+    
+    void resized() override;
+    
+    void timerCallback() override;
+    
+    bool keyPressed(const KeyPress& key, Component*) override;
+    bool keyStateChanged(bool isKeyDown, Component*) override;
+    
+    void zoom(bool zoomIn);
+    
+    LevelMeter* levelMeter;
+    MidiBlinker* midiBlinker;
+
+    std::unique_ptr<TextButton> bypassButton;
+    std::unique_ptr<TextButton> lockButton;
+    std::unique_ptr<TextButton> connectionStyleButton;
+    std::unique_ptr<TextButton> connectionPathfind;
+
+    std::unique_ptr<TextButton> zoomIn;
+    std::unique_ptr<TextButton> zoomOut;
+    
+    Label zoomLabel;
+    
+    Slider volumeSlider;
+    
+    Value locked;
+    Value commandLocked;
+    
+    Value zoomScale;
+    
+    Value connectionStyle;
+    
+    static constexpr int statusbarHeight = 25;
+    
+    std::unique_ptr<ButtonParameterAttachment> enableAttachment;
+    std::unique_ptr<SliderParameterAttachment> volumeAttachment;
+    
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Statusbar)
+};
