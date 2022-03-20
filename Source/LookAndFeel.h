5a32d4f38fb69c7d5885ef72da398ce81a41696d
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-03-20 01:14:43 +0100

    Fixed connection bug

--- Source/LookAndFeel.h
+++ Source/LookAndFeel.h
@@ -356,12 +356,12 @@ struct PlugDataDarkLook : public PlugDataLook
         
         if (!Desktop::canUseSemiTransparentWindows() && JUCEApplicationBase::isStandaloneApp())
         {
-            g.fillAll(findColour(ResizableWindow::backgroundColourId));
+            g.fillAll(findColour(ComboBox::backgroundColourId));
         }
         
         // On linux, the canUseSemiTransparentWindows flag sometimes incorrectly returns true
 #ifdef JUCE_LINUX
-        g.fillAll(findColour(ResizableWindow::backgroundColourId));
+        g.fillAll(findColour(ComboBox::backgroundColourId));
 #endif
 
         auto bounds = Rectangle<float>(2, 2, width - 4, height - 4);
