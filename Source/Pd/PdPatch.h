ed0c301b0f0e97475488aa058fa16ca9eafe9a3d
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-05-18 21:07:26 +0200

    Final gui component tweaks

--- Source/Pd/PdPatch.h
+++ Source/Pd/PdPatch.h
@@ -132,13 +132,13 @@ class Patch
     // Initialisation parameters for GUI objects
     // Taken from pd save files, this will make sure that it directly initialises objects with the right parameters
     static inline const std::map<String, String> guiDefaults = {{"tgl", "23 0 empty empty empty 17 7 0 10 bgColour fgColour lblColour 0 1"},
-                                                                {"hsl", "128 15 0 127 0 0 empty empty empty -2 -8 0 10 bgColour lnColour lblColour 0 1"},
-                                                                {"vsl", "15 128 0 127 0 0 empty empty empty 0 -9 0 10 bgColour lnColour lblColour 0 1"},
+                                                                {"hsl", "128 15 0 127 0 0 empty empty empty -2 -8 0 10 bgColour fgColour lblColour 0 1"},
+                                                                {"vsl", "15 128 0 127 0 0 empty empty empty 0 -9 0 10 bgColour fgColour lblColour 0 1"},
                                                                 {"bng", "23 250 50 0 empty empty empty 17 7 0 10 bgColour fgColour lblColour"},
-                                                                {"nbx", "4 19 -1e+37 1e+37 0 0 empty empty empty 0 -8 0 10 bgColour lblColour lblColour 0 256"},
-                                                                {"hradio", "18 1 0 8 empty empty empty 0 -8 0 10 bgColour lnColour lblColour 0"},
-                                                                {"vradio", "18 1 0 8 empty empty empty 0 -8 0 10 bgColour lnColour lblColour 0"},
-                                                                {"cnv", "15 100 60 empty empty empty 20 12 0 14 #8b949e lblColour"},
+                                                                {"nbx", "4 19 -1e+37 1e+37 0 0 empty empty empty 0 -8 0 10 bgColour fgColour lblColour 0 256"},
+                                                                {"hradio", "18 1 0 8 empty empty empty 0 -8 0 10 bgColour fgColour lblColour 0"},
+                                                                {"vradio", "18 1 0 8 empty empty empty 0 -8 0 10 bgColour fgColour lblColour 0"},
+                                                                {"cnv", "15 100 60 empty empty empty 20 12 0 14 #8b949e fgColour"},
                                                                 {"vu", "15 120 empty empty -1 -8 0 10 bgColour lblColour 1 0"},
                                                                 {"floatatom", "5 -3.40282e+38 3.40282e+38 0 empty - - 12"}};
 
