01dac448fe3762575b387258e2b1e51323c1c012
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-05-29 18:52:57 +0200

    Simplified build script

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -2,24 +2,6 @@
 
 rm -f PlugData-$1
 cp -r Plugins PlugData
-
-mkdir PlugData/LV2/PlugData.lv2
-
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-./Plugins/LV2/lv2_file_generator PlugData/LV2/PlugData_LV2.so PlugData
-mv PlugData/LV2/PlugData_LV2.so PlugData/LV2/PlugData.lv2/PlugData.so
-
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-./Plugins/LV2/lv2_file_generator PlugData/LV2/PlugData_LV2.dylib PlugData
-mv PlugData/LV2/PlugData_LV2.dylib PlugData/LV2/PlugData.lv2/PlugData.dylib
-fi
-
-mv manifest.ttl PlugData/LV2/PlugData.lv2/manifest.ttl
-mv presets.ttl PlugData/LV2/PlugData.lv2/presets.ttl
-mv PlugData.ttl PlugData/LV2/PlugData.lv2/PlugData.ttl
-
-rm PlugData/LV2/lv2_file_generator
-
 cp README.md PlugData/README.md
 cp LICENSE PlugData/LICENSE.txt
 
