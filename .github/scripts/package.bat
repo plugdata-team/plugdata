28dcd7474e54d3cce6749a518732981ac782537c
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-05-29 18:52:57 +0200

    Simplified build script

--- .github/scripts/package.bat
+++ .github/scripts/package.bat
@@ -2,32 +2,6 @@
 del PlugData-%1.zip /Q
 xcopy Plugins PlugData /S /H /E /I
 
-
-cd PlugData\LV2
-md PlugData.lv2
-call lv2_file_generator.exe PlugData_LV2.dll PlugData
-
-move PlugData_LV2.dll PlugData.lv2\PlugData.dll
-move manifest.ttl PlugData.lv2\manifest.ttl
-move presets.ttl PlugData.lv2\presets.ttl
-move PlugData.ttl PlugData.lv2\PlugData.ttl
-
-del lv2_file_generator.exe /F
-
-cd ..\..
-
-move PlugData\VST3\PlugData.vst3\Contents\x86-win\PlugData.vst3 PlugData\VST3\PlugData_temp.vst3
-move PlugData\VST3\PlugData.vst3\Contents\x86_64-win\PlugData.vst3 PlugData\VST3\PlugData_temp.vst3
-rd PlugData\VST3\PlugData.vst3\ /S /Q
-move PlugData\VST3\PlugData_temp.vst3 PlugData\VST3\PlugData.vst3
-
-move PlugData\VST3\PlugDataFx.vst3\Contents\x86-win\PlugDataFx.vst3 PlugData\VST3\PlugDataFx_temp.vst3
-move PlugData\VST3\PlugDataFx.vst3\Contents\x86_64-win\PlugDataFx.vst3 PlugData\VST3\PlugDataFx_temp.vst3
-rd PlugData\VST3\PlugDataFx.vst3\ /S /Q
-move PlugData\VST3\PlugDataFx_temp.vst3 PlugData\VST3\PlugDataFx.vst3
-
-
-
 copy README.md PlugData\README.md
 copy ChangeLog.md PlugData\ChangeLog.md
 copy LICENSE PlugData\LICENSE.txt
