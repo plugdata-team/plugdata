9f53c87112e5be643ce6fc2c597c61e62ea615f7
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-05-30 03:25:41 +0200

    Restored windows build script

--- .github/scripts/package.bat
+++ .github/scripts/package.bat
@@ -2,6 +2,16 @@
 del PlugData-%1.zip /Q
 xcopy Plugins PlugData /S /H /E /I
 
+move PlugData\VST3\PlugData.vst3\Contents\x86-win\PlugData.vst3 PlugData\VST3\PlugData_temp.vst3
+move PlugData\VST3\PlugData.vst3\Contents\x86_64-win\PlugData.vst3 PlugData\VST3\PlugData_temp.vst3
+rd PlugData\VST3\PlugData.vst3\ /S /Q
+move PlugData\VST3\PlugData_temp.vst3 PlugData\VST3\PlugData.vst3
+
+move PlugData\VST3\PlugDataFx.vst3\Contents\x86-win\PlugDataFx.vst3 PlugData\VST3\PlugDataFx_temp.vst3
+move PlugData\VST3\PlugDataFx.vst3\Contents\x86_64-win\PlugDataFx.vst3 PlugData\VST3\PlugDataFx_temp.vst3
+rd PlugData\VST3\PlugDataFx.vst3\ /S /Q
+move PlugData\VST3\PlugDataFx_temp.vst3 PlugData\VST3\PlugDataFx.vst3
+
 copy README.md PlugData\README.md
 copy ChangeLog.md PlugData\ChangeLog.md
 copy LICENSE PlugData\LICENSE.txt
