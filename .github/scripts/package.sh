4e7bb39e882f22bfd79d59c7f86b1a7bbd1780e5
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-07-06 02:27:39 +0200

    Maybe sign like this?

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -8,11 +8,11 @@ cp LICENSE PlugData/LICENSE.txt
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   #sudo chmod -R 777 ./Plugins/
-  #codesign -f -v -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./Plugins/VST3/*.vst3 --timestamp
-  #codesign -f -v -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./Plugins/Standalone/*.app --timestamp
-  #codesign -f -v -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./Plugins/AU/*.component --timestamp
-  #codesign -f -v -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./Plugins/LV2/PlugData.lv2/libPlugData.so --timestamp
-  #codesign -f -v -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./Plugins/LV2/PlugDataFx.lv2/libPlugDataFx.so --timestamp
+  /usr/bin/codesign --force --deep -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./Plugins/VST3/*.vst3 --timestamp
+  /usr/bin/codesign --force --deep -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./Plugins/Standalone/*.app --timestamp
+  /usr/bin/codesign --force --deep -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./Plugins/AU/*.component --timestamp
+  /usr/bin/codesign --force --deep -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./Plugins/LV2/PlugData.lv2/libPlugData.so --timestamp
+  /usr/bin/codesign --force --deep -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./Plugins/LV2/PlugDataFx.lv2/libPlugDataFx.so --timestamp
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
  mv PlugData PlugData-$1
 fi
