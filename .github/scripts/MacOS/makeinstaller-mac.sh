b6ffd0ddc2fcf2cb344384b53bf46f14d4d05453
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-07-06 03:15:37 +0200

    Try to notarise but continue on error

--- .github/scripts/MacOS/makeinstaller-mac.sh
+++ .github/scripts/MacOS/makeinstaller-mac.sh
@@ -133,5 +133,5 @@ rm -r $PKG_DIR
 
 productsign -s "Developer ID Installer: Timothy Schoen (7SV7JPRR2L)" ${PRODUCT_NAME}.pkg ${PRODUCT_NAME}-MacOS-Universal.pkg
 
-xcrun notarytool submit ./PlugData-MacOS-Universal.pkg --keychain-profile "installer-keychain" --wait
-xcrun stapler staple "PlugData-MacOS-Universal.dmg"
+xcrun notarytool submit ./PlugData-MacOS-Universal.pkg --keychain-profile "installer-keychain" --wait || true
+xcrun stapler staple "PlugData-MacOS-Universal.dmg" || true
