7cf0e01740ba9e337329749c7d32862fea40047e
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-07-07 16:36:40 +0200

    Simplified workflow

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -7,18 +7,10 @@ cp README.md PlugData/README.md
 cp LICENSE PlugData/LICENSE.txt
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sudo chmod -R 777 ./Plugins/
-
-# Sign app with hardened runtime and audio entitlement
-/usr/bin/codesign --force -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" --options runtime --entitlements ./Resources/Entitlements.plist ./PlugData/Standalone/*.app
-
-# Sign plugins
-/usr/bin/codesign --force -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./PlugData/VST3/*.vst3
-/usr/bin/codesign --force -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./PlugData/AU/*.component
-/usr/bin/codesign --force -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./PlugData/LV2/PlugData.lv2/libPlugData.so
-/usr/bin/codesign --force -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./PlugData/LV2/PlugDataFx.lv2/libPlugDataFx.so
-
+  ./.github/script/MacOS/makeinstaller-mac.sh ${{github.ref_name}}
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
  mv PlugData PlugData-$1
+else
+   ./.github/script/Windows/makeinstaller-win.sh ${{github.ref_name}} $1
 fi
 
