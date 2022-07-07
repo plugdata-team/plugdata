75311f9a72d5d2bb82dabd3ab0cf1b0f5486db6f
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-07-07 15:08:31 +0200

    Fix notarisation issue

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -9,8 +9,11 @@ cp LICENSE PlugData/LICENSE.txt
 if [[ "$OSTYPE" == "darwin"* ]]; then
   sudo chmod -R 777 ./Plugins/
 
+# Sign app with hardened runtime and audio entitlement
+/usr/bin/codesign --force -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" --options runtime --entitlements ./Resources/Entitlements.plist ./PlugData/Standalone/*.app
+
+# Sign plugins
 /usr/bin/codesign --force -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./PlugData/VST3/*.vst3
-/usr/bin/codesign --force -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./PlugData/Standalone/*.app
 /usr/bin/codesign --force -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./PlugData/AU/*.component
 /usr/bin/codesign --force -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./PlugData/LV2/PlugData.lv2/libPlugData.so
 /usr/bin/codesign --force -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./PlugData/LV2/PlugDataFx.lv2/libPlugDataFx.so
@@ -18,3 +21,4 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
  mv PlugData PlugData-$1
 fi
+
