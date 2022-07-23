4690c6823278f35fbda4a73b103a97cb37ead78d
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-07-23 17:35:15 +0200

    remove trash

--- Libraries/ELSE/sfont~/fluidsynth_deps/libogg-1.3.5/confdefs.h
+++
@@ -1,7 +0,0 @@
-/* confdefs.h */
-#define PACKAGE_NAME "libogg"
-#define PACKAGE_TARNAME "libogg"
-#define PACKAGE_VERSION "1.3.5"
-#define PACKAGE_STRING "libogg 1.3.5"
-#define PACKAGE_BUGREPORT "ogg-dev@xiph.org"
-#define PACKAGE_URL ""
