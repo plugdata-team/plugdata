4008d62e1b330715bc9ffcc39671615b930aacc2
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-08-22 19:59:20 +0200

    Fixed fonts on Windows, fixed crash

--- Source/LookAndFeel.h
+++ Source/LookAndFeel.h
@@ -70,7 +70,7 @@ enum PlugDataColour
 
 struct Resources
 {
-    Typeface::Ptr defaultTypeface = Typeface::createSystemTypefaceFor(BinaryData::InterRegular_otf, BinaryData::InterRegular_otfSize);
+    Typeface::Ptr defaultTypeface = Typeface::createSystemTypefaceFor(BinaryData::InterRegular_ttf, BinaryData::InterRegular_ttfSize);
 
     Typeface::Ptr iconTypeface = Typeface::createSystemTypefaceFor(BinaryData::PlugDataFont_ttf, BinaryData::PlugDataFont_ttfSize);
 };
