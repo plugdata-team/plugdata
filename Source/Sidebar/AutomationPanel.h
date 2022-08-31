be512004bce2459c4a3b413af8500f37983c0d67
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-08-31 19:37:15 +0200

    Some automation improvements

--- Source/Sidebar/AutomationPanel.h
+++ Source/Sidebar/AutomationPanel.h
@@ -53,7 +53,7 @@ struct AutomationComponent : public Component {
 #else
             auto* param = pd->parameters.getParameter("param" + String(p + 1));
             auto range = param->getNormalisableRange().getRange();
-            attachments.add(new SliderParameterAttachment(*param, *slider, nullptr));
+            //attachments.add(new SliderParameterAttachment(*param, *slider, nullptr));
 #endif
         }
     }
