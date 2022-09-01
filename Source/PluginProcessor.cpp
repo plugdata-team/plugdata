6ac79fd059056e30b6d0053161710ade1173f2c5
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-09-01 13:19:58 +0200

    Fixed Windows compilation

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -208,9 +208,9 @@ void PlugDataAudioProcessor::initialiseFilesystem()
         
         try
         {
-            std::filesystem::create_directory_symlink(abstractionsPath, library.getChildFile("Abstractions").getFullPathName().replaceAllOccurancesOf("/", "\\").toStdString());
-            std::filesystem::create_directory_symlink(documentationPath, library.getChildFile("Documentation").getFullPathName().replaceAllOccurancesOf("/", "\\").toStdString());
-            std::filesystem::create_directory_symlink(dekenPath, library.getChildFile("Deken").getFullPathName().replaceAllOccurancesOf("/", "\\").toStdString());
+            std::filesystem::create_directory_symlink(abstractionsPath, library.getChildFile("Abstractions").getFullPathName().replace("/", "\\").toStdString());
+            std::filesystem::create_directory_symlink(documentationPath, library.getChildFile("Documentation").getFullPathName().replace("/", "\\").toStdString());
+            std::filesystem::create_directory_symlink(dekenPath, library.getChildFile("Deken").getFullPathName().replace("/", "\\").toStdString());
         }
         catch (...)
         {
