13890eec59a8d25a3af13fe6b2cefe06395259f5
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-10-27 00:51:43 +0200

    Better code for creating directory junctions on Windows

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -4,6 +4,65 @@
  // WARRANTIES, see the file, "LICENSE.txt," in this distribution.
  */
 
+
+#ifdef JUCE_WINDOWS
+namespace Win32
+{
+
+#include <windows.h>
+#include <shlobj.h>
+
+static void createJunction(std::string szJunctionPtr, std::string szPathPtr) {
+    
+    auto szJunction = (LPCTSTR)szJunctionPtr.c_str();
+    auto szPath = (LPCTSTR)szPathPtr.c_str();
+    
+    BYTE buf[sizeof(REPARSE_MOUNTPOINT_DATA_BUFFER) + MAX_PATH * sizeof(WCHAR)];
+    REPARSE_MOUNTPOINT_DATA_BUFFER& ReparseBuffer = (REPARSE_MOUNTPOINT_DATA_BUFFER&)buf;
+    char szTarget[MAX_PATH] = "\\??\\";
+    
+    strcat(szTarget, szPath);
+    strcat(szTarget, "\\");
+    
+    if (!::CreateDirectory(szJunction, NULL)) throw ::GetLastError();
+    
+    // Obtain SE_RESTORE_NAME privilege (required for opening a directory)
+    HANDLE hToken = NULL;
+    TOKEN_PRIVILEGES tp;
+    try {
+        if (!::OpenProcessToken(::GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken)) throw ::GetLastError();
+        if (!::LookupPrivilegeValue(NULL, SE_RESTORE_NAME, &tp.Privileges[0].Luid))  throw ::GetLastError();
+        tp.PrivilegeCount = 1;
+        tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+        if (!::AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), NULL, NULL))  throw ::GetLastError();
+    }
+    catch (DWORD) { }   // Ignore errors
+    if (hToken) ::CloseHandle(hToken);
+    
+    HANDLE hDir = ::CreateFile(szJunction, GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_FLAG_OPEN_REPARSE_POINT | FILE_FLAG_BACKUP_SEMANTICS, NULL);
+    if (hDir == INVALID_HANDLE_VALUE) throw ::GetLastError();
+    
+    memset(buf, 0, sizeof(buf));
+    ReparseBuffer.ReparseTag = IO_REPARSE_TAG_MOUNT_POINT;
+    int len = ::MultiByteToWideChar(CP_ACP, 0, szTarget, -1, ReparseBuffer.ReparseTarget, MAX_PATH);
+    ReparseBuffer.ReparseTargetMaximumLength = (len--) * sizeof(WCHAR);
+    ReparseBuffer.ReparseTargetLength = len * sizeof(WCHAR);
+    ReparseBuffer.ReparseDataLength = ReparseBuffer.ReparseTargetLength + 12;
+    
+    DWORD dwRet;
+    if (!::DeviceIoControl(hDir, FSCTL_SET_REPARSE_POINT, &ReparseBuffer, ReparseBuffer.ReparseDataLength+REPARSE_MOUNTPOINT_HEADER_SIZE, NULL, 0, &dwRet, NULL)) {
+        DWORD dr = ::GetLastError();
+        ::CloseHandle(hDir);
+        ::RemoveDirectory(szJunction);
+        throw dr;
+    }
+    
+    ::CloseHandle(hDir);
+}
+
+};
+#endif
+
 #include <clocale>
 #include "PluginProcessor.h"
 
@@ -210,6 +269,8 @@ void PlugDataAudioProcessor::initialiseFilesystem()
         deken.createDirectory();
         
 #if JUCE_WINDOWS
+        
+        
         // Get paths that need symlinks
         auto abstractionsPath = appDir.getChildFile("Abstractions").getFullPathName().replaceCharacters("/", "\\");
         auto documentationPath = appDir.getChildFile("Documentation").getFullPathName().replaceCharacters("/", "\\");
@@ -217,22 +278,12 @@ void PlugDataAudioProcessor::initialiseFilesystem()
         
         // The mklink /J command creates a directory junction. This is the closest thing I can get to a directory symlink on Windows
         // Regular symlinks can only created by administrators
-        auto abstractionsCommand = "cmd.exe /k mklink /J " + library.getChildFile("Abstractions").getFullPathName().replaceCharacters("/", "\\") + " " + abstractionsPath;
-        auto documentationCommand = "cmd.exe /k mklink /J " + library.getChildFile("Documentation").getFullPathName().replaceCharacters("/", "\\") + " " + documentationPath;
-        auto dekenCommand = "cmd.exe /k mklink /J " + library.getChildFile("Deken").getFullPathName().replaceCharacters("/", "\\") + " " + dekenPath;
         
-        // Execute junction command
-        // TODO: write real C++ code for creating junctions
-#if _WIN64
-        // For some reason, this only links with 64-bit targets
-        WinExec(abstractionsCommand.toRawUTF8(), 0);
-        WinExec(documentationCommand.toRawUTF8(), 0);
-        WinExec(dekenCommand.toRawUTF8(), 0);
-#else   
-        system(abstractionsCommand.fromFirstOccurrenceOf("/k", false, false).toRawUTF8());
-        system(documentationCommand.fromFirstOccurrenceOf("/k", false, false).toRawUTF8());
-        system(dekenCommand.fromFirstOccurrenceOf("/k", false, false).toRawUTF8());
-#endif
+        createJunction(library.getChildFile("Abstractions").getFullPathName().replaceCharacters("/", "\\").toStdString(), abstractionsPath.toStdString());
+        
+        createJunction(library.getChildFile("Documentation").getFullPathName().replaceCharacters("/", "\\").toStdString(), documentationPath.toStdString());
+        
+        createJunction(library.getChildFile("Deken").getFullPathName().replaceCharacters("/", "\\").toStdString(), dekenPath.toStdString());
 
 #else
         appDir.getChildFile("Abstractions").createSymbolicLink(library.getChildFile("Abstractions"), true);
