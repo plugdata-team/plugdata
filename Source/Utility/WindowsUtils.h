b8f7ea7055ce01c35319af7b2b05138b6f15f6e7
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-19 18:57:08 +0100

    Added missing file

---
+++ Source/Utility/WindowsUtils.h
@@ -0,0 +1,10 @@
+/*
+ // Copyright (c) 2021-2022 Timothy Schoen
+ // For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ // WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+ */
+
+
+void createJunction(std::string from, std::string to);
+void createHardLink(std::string from, std::string to);
+bool runAsAdmin(std::string file, std::string lpParameters, void* hWnd);
