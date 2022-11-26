3f4b98890610aed88a89fc95df4a63ccfc622313
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-11-26 17:57:21 +0100

    Added missing file

---
+++ Source/Dialogs/AdvancedSettingsPanel.h
@@ -0,0 +1,41 @@
+/*
+ // Copyright (c) 2021-2022 Timothy Schoen
+ // For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ // WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+ */
+
+#pragma once
+
+
+struct AdvancedSettingsPanel : public Component
+{
+    
+    AdvancedSettingsPanel(ValueTree tree) : settingsTree(tree)
+    {
+        nativeTitlebar.referTo(settingsTree.getPropertyAsValue("NativeWindow", nullptr));
+        reloadPatch.referTo(settingsTree.getPropertyAsValue("ReloadLastPatch", nullptr));
+        
+        useNativeTitlebar.reset(new PropertiesPanel::BoolComponent("Use native titlebar", nativeTitlebar, {"No", "Yes"}));
+        
+        reloadLastOpenedPatch.reset(new PropertiesPanel::BoolComponent("Reload last opened patch startup", reloadPatch, {"No", "Yes"}));
+        
+        addAndMakeVisible(*useNativeTitlebar);
+        addAndMakeVisible(*reloadLastOpenedPatch);
+    }
+    
+    void resized() override
+    {
+        auto bounds = getLocalBounds();
+        useNativeTitlebar->setBounds(bounds.removeFromTop(23));
+        reloadLastOpenedPatch->setBounds(bounds.removeFromTop(23));
+    }
+    
+    ValueTree settingsTree;
+
+    Value nativeTitlebar;
+    Value reloadPatch;
+
+    std::unique_ptr<PropertiesPanel::BoolComponent> useNativeTitlebar;
+    std::unique_ptr<PropertiesPanel::BoolComponent> reloadLastOpenedPatch;
+
+};
