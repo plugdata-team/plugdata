85dd5bb6f351cbe6f6aace5bac2f2d2200f363e1
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-12-07 20:08:43 +0100

    Windows fixes

--- Libraries/cyclone/cyclone_objects/binaries/control/anal.c
+++ Libraries/cyclone/cyclone_objects/binaries/control/anal.c
@@ -1,3 +1,4 @@
+
 /* Copyright (c) 2002-2003 krzYszcz and others.
  * For information on usage and redistribution, and for a DISCLAIMER OF ALL
  * WARRANTIES, see the file, "LICENSE.txt," in this distribution.  */
