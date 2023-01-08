4a7ea31aa322d1754e6660d1fcac3491a15f2a1a
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-01-08 17:56:27 +0100

    Fixing FTP on Windows

--- .github/scripts/upload-ftp.sh
+++ .github/scripts/upload-ftp.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 HOST='glyphpress.com'
 USER=${FTP_USERNAME}
 PASSWD=${FTP_PASSWORD}
@@ -12,6 +11,40 @@ then
 tar -czvf "${FILE}.tar.gz" ${FILE}
 FILE="${FILE}.tar.gz"
 fi
+
+UNAME=$(uname)
+if [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW*  || "$UNAME" == MSYS* ]] ; then
+
+echo "Windows detected"
+
+cat > ./plugdata.ftp <<END_SCRIPT
+open $HOST
+$USER
+$PASSWD
+binary
+delete $FILE
+quit
+END_SCRIPT
+
+ftp -s:plugdata.ftp
+
+# Wait 2 minutes to ensure the file is gone
+sleep 40
+
+rm plugdata.ftp
+
+cat > ./plugdata.ftp <<END_SCRIPT
+open $HOST
+$USER
+$PASSWD
+binary
+put $FILE
+quit
+END_SCRIPT
+
+ftp -s:plugdata.ftp
+
+else
 
 ftp -p -inv $HOST <<END_SCRIPT
 quote USER $USER
@@ -34,4 +67,5 @@ put $FILE
 quit
 END_SCRIPT
 
+fi
 exit 0
