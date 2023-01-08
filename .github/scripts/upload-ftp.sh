934fbb30d3d85a555d41525aa490c8e819203eb1
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-01-08 23:09:45 +0100

    Use curl for uploads

--- .github/scripts/upload-ftp.sh
+++ .github/scripts/upload-ftp.sh
@@ -1,75 +1,13 @@
 #!/bin/sh
 
 HOST=glyphpress.com
-USER=${FTP_USERNAME}
-PASSWD=${FTP_PASSWORD}
 FILE=$1
 INFO_FILE=${FILE}.txt
 DATE=$(date)
 
 UNAME=$(uname)
-if [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW*  || "$UNAME" == MSYS* ]] ; then
 
-cat > INFO_FILE <<END_FILE
-$DATE
-$GIT_HASH
-END_FILE
-
-cat > ./plugdata.ftp <<END_SCRIPT
-open $HOST
-user $USER
-password $PASSWD
-binary
-delete $FILE
-delete $INFO_FILE
-quit
-END_SCRIPT
-
-ftp -s:plugdata.ftp -n
-
-# Wait 2 minutes to ensure the file is gone
-sleep 40
-
-rm plugdata.ftp
-
-cat > ./plugdata.ftp <<END_SCRIPT
-open $HOST
-user $USER
-password $PASSWD
-binary
-put $FILE
-put $INFO_FILE
-quit
-END_SCRIPT
-
-ftp -s:plugdata.ftp -n
-rm plugdata.ftp
-
-else
-
-ftp -p -inv $HOST <<END_SCRIPT
-quote USER $USER
-quote PASS $PASSWD
-quote PASV
-binary
-delete $FILE
-delete $INFO_FILE
-quit
-END_SCRIPT
-
-# Wait 2 minutes to ensure the file is gone
-sleep 40
-
-ftp -p -inv $HOST <<END_SCRIPT
-quote USER $USER
-quote PASS $PASSWD
-quote PASV
-binary
-put $FILE
-put $INFO_FILE
-quit
-END_SCRIPT
-
-fi
+curl -T ${FILE} ftp://glyphpress.com/plugdata/${FILE} --user ${FTP_USERNAME}:${FTP_PASSWORD}
+curl -T ${INFO_FILE} ftp://glyphpress.com/plugdata/${INFO_FILE} --user ${FTP_USERNAME}:${FTP_PASSWORD}
 
 exit 0
