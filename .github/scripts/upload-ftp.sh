d43c08f64c640b7516da8ad5b54ea7b1411c02c4
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-01-08 23:14:10 +0100

    FTP upload fixes

--- .github/scripts/upload-ftp.sh
+++ .github/scripts/upload-ftp.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-HOST=glyphpress.com
 FILE=$1
 INFO_FILE=${FILE}.txt
 DATE=$(date)
 
-UNAME=$(uname)
+cat > INFO_FILE <<END_FILE
+$DATE
+$GIT_HASH
+END_FILE
 
-curl -T ${FILE} ftp://glyphpress.com/plugdata/${FILE} --user ${FTP_USERNAME}:${FTP_PASSWORD}
-curl -T ${INFO_FILE} ftp://glyphpress.com/plugdata/${INFO_FILE} --user ${FTP_USERNAME}:${FTP_PASSWORD}
+curl -T ${FILE} ftp://glyphpress.com/${FILE} --user ${FTP_USERNAME}:${FTP_PASSWORD}
+curl -T ${INFO_FILE} ftp://glyphpress.com/${INFO_FILE} --user ${FTP_USERNAME}:${FTP_PASSWORD}
 
 exit 0
