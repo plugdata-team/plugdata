7289829cda80fbb082d87d945bb64375fe36cbd0
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-01-09 00:29:21 +0100

    Keep track of latest hash

--- .github/scripts/upload-ftp.sh
+++ .github/scripts/upload-ftp.sh
@@ -3,13 +3,19 @@
 FILE=$1
 INFO_FILE=${FILE}.txt
 DATE=$(date -u +"%d-%m-%y %H:%M UTC")
+LATEST_HASH=$(git ls-remote https://github.com/plugdata-team/plugdata.git HEAD)
 
 cat > $INFO_FILE <<END_FILE
 $DATE
 $GIT_HASH
 END_FILE
 
+cat > latest.txt <<END_FILE
+$LATEST_HASH
+END_FILE
+
 curl -T ./${FILE} ftp://glyphpress.com/${FILE} --user ${FTP_USERNAME}:${FTP_PASSWORD}
 curl -T ./${INFO_FILE} ftp://glyphpress.com/${INFO_FILE} --user ${FTP_USERNAME}:${FTP_PASSWORD}
+curl -T ./latest.txt ftp://glyphpress.com/latest.txt --user ${FTP_USERNAME}:${FTP_PASSWORD}
 
 exit 0
