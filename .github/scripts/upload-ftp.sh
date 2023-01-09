b0bae3178f1d0913ad8fde253efb409f854fc114
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-01-09 02:38:46 +0100

    Make sure we don't overwrite files

--- .github/scripts/upload-ftp.sh
+++ .github/scripts/upload-ftp.sh
@@ -2,7 +2,11 @@
 
 FILE=$1
 INFO_FILE=${FILE}.txt
+TIMESTAMP_FILE=${FILE}_timestamp.txt
+
 DATE=$(date -u +"%d-%m-%y %H:%M UTC")
+COMMIT_TIMESTAMP=$(git show -s --format=%ct HEAD)
+
 LATEST_HASH=$(git ls-remote https://github.com/plugdata-team/plugdata.git HEAD)
 
 cat > $INFO_FILE <<END_FILE
@@ -10,12 +14,34 @@ $DATE
 $GIT_HASH
 END_FILE
 
+cat > $TIMESTAMP_FILE <<END_FILE
+$COMMIT_TIMESTAMP
+END_FILE
+
 cat > latest.txt <<END_FILE
 $LATEST_HASH
 END_FILE
 
+# Get the last timestamp
+curl https://glyphpress.com/plugdata/${TIMESTAMP_FILE} --output ./last_timestamp.txt
+
+if [ -s last_timestamp.txt ]; then
+    LAST_TIMESTAMP=$(<last_timestamp.txt)
+else
+    LAST_TIMESTAMP="0"
+fi
+
+# Make sure that a later commit didn't finish earlier than this one
+if [ COMMIT_TIMESTAMP -gt LAST_TIMESTAMP ]; then
+
+# Upload files and additional information
 curl -T ./${FILE} ftp://glyphpress.com/${FILE} --user ${FTP_USERNAME}:${FTP_PASSWORD}
 curl -T ./${INFO_FILE} ftp://glyphpress.com/${INFO_FILE} --user ${FTP_USERNAME}:${FTP_PASSWORD}
+curl -T ./${TIMESTAMP_FILE} ftp://glyphpress.com/${TIMESTAMP_FILE} --user ${FTP_USERNAME}:${FTP_PASSWORD}
+fi
+
+# Update the latest version
 curl -T ./latest.txt ftp://glyphpress.com/latest.txt --user ${FTP_USERNAME}:${FTP_PASSWORD}
+
 
 exit 0
