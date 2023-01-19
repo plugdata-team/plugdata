688b29144b2eec37066f9802e128cbb566ed3ffd
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-01-19 23:55:17 +0100

    Cleaned up internal synth

--- Source/Utility/InternalSynth.h
+++ Source/Utility/InternalSynth.h
@@ -4,23 +4,22 @@
  // WARRANTIES, see the file, "LICENSE.txt," in this distribution.
  */
 
+
 #include "FluidLite/include/fluidlite.h"
 #include "FluidLite/src/fluid_sfont.h"
 
+
+// InternalSynth is an internal General MIDI synthesizer that can be used as a MIDI output device
+// The goal is to get something similar to the "AU DLS Synth" in Max/MSP on macOS, but cross-platform
+// Since fluidsynth is alraedy included for the sfont~ object, we can reuse it here to read a GM soundfont
+
 struct InternalSynth
 {
-    fluid_synth_t* synth;
-    fluid_settings_t* settings;
-    fluid_sfont_t* sfont;
-    fluid_preset_t* preset;
+    ~InternalSynth() {
         
-    InternalSynth() {
-
+        if(synth) delete_fluid_synth(synth);
+        if(settings) delete_fluid_settings(settings);
     }
-    
-    bool ready = false;
-    
-    AudioBuffer<float> internalBuffer;
     
     void prepare(int sampleRate, int blockSize, int numChannels)
     {
@@ -28,9 +27,13 @@ struct InternalSynth
         
         auto soundFont = homeDir.getChildFile("Library").getChildFile("Extra").getChildFile("GS").getChildFile("FluidR3Mono_GM.sf3");
         
+        internalBuffer.setSize(2, blockSize);
+        
+        // Check if soundfont exists to prevent crashing
         if(soundFont.existsAsFile()) {
             auto pathName = soundFont.getFullPathName();
             
+            // Initialise fluidsynth
             settings = new_fluid_settings();
             fluid_settings_setint(settings, "synth.ladspa.active", 0);
             fluid_settings_setint(settings, "synth.midi-channels", 16);
@@ -39,13 +42,11 @@ struct InternalSynth
             fluid_settings_setnum(settings, "synth.sample-rate", sampleRate);
             synth = new_fluid_synth(settings); // Create fluidsynth instance:
             
+            // Load the soundfont
             int ret = fluid_synth_sfload(synth, pathName.toRawUTF8(), 0);
-            
-            internalBuffer.setSize(2, blockSize);
             
             if(ret >= 0){
                 fluid_synth_program_reset(synth);
-                sfont = fluid_synth_get_sfont_by_id(synth, ret);
             }
             
             ready = true;
@@ -56,8 +57,7 @@ struct InternalSynth
         
         if(!ready) return;
         
-        fluid_synth_write_float(synth, buffer.getNumSamples(), internalBuffer.getWritePointer(0), 0, 1, internalBuffer.getWritePointer(1), 0, 1);
-        
+        // Pass MIDI messages to fluidsynth
         for (auto const& event : midiMessages) {
             auto const message = event.getMessage();
             
@@ -86,40 +86,19 @@ struct InternalSynth
             }
         }
         
+        // Run audio through fluidsynth
+        fluid_synth_write_float(synth, buffer.getNumSamples(), internalBuffer.getWritePointer(0), 0, 1, internalBuffer.getWritePointer(1), 0, 1);
+        
         buffer.addFrom(0, 0, internalBuffer, 0, 0, buffer.getNumSamples());
         buffer.addFrom(1, 0, internalBuffer, 1, 0, buffer.getNumSamples());
-        
-        /*
-
-        case 0xA0: // group 160-175 (POLYPHONIC AFTERTOUCH)
-            SETFLOAT(&x->x_at[0], val); // aftertouch pressure value
-            SETFLOAT(&x->x_at[1], (t_float)x->x_data); // key
-            SETFLOAT(&x->x_at[2], (t_float)x->x_channel);
-            sfont_polytouch(x, &s_list, 3, x->x_at);
-            break;
-        case 0xB0: // group 176-191 (CONTROL CHANGE)
-            SETFLOAT(&x->x_at[0], val); // control value
-            SETFLOAT(&x->x_at[1], (t_float)x->x_data); // control number
-            SETFLOAT(&x->x_at[2], (t_float)x->x_channel);
-            sfont_control_change(x, &s_list, 3, x->x_at);
-            break;
-        case 0xC0: // group 192-207 (PROGRAM CHANGE)
-            SETFLOAT(&x->x_at[0], val); // control value
-            SETFLOAT(&x->x_at[1], (t_float)x->x_channel);
-            sfont_program_change(x, &s_list, 2, x->x_at);
-            break;
-        case 0xD0: // group 208-223 (AFTERTOUCH)
-            SETFLOAT(&x->x_at[0], val); // control value
-            SETFLOAT(&x->x_at[1], (t_float)x->x_channel);
-            sfont_aftertouch(x, &s_list, 2, x->x_at);
-            break;
-        case 0xE0: // group 224-239 (PITCH BEND)
-            SETFLOAT(&x->x_at[0], (val << 7) + x->x_data);
-            SETFLOAT(&x->x_at[1], x->x_channel);
-            sfont_pitch_bend(x, &s_list, 2, x->x_at);
-            break;
-        default:
-            break;
-        } */
     }
+    
+private:
+    // Fluidsynth state
+    fluid_synth_t* synth = nullptr;
+    fluid_settings_t* settings = nullptr;
+        
+    bool ready = false;
+    
+    AudioBuffer<float> internalBuffer;
 };
