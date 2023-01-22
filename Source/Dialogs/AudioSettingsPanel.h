744a0c2767a34fa6a8d9036728f4fb22904a7327
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-01-22 15:50:28 +0100

    Plugin compilation fix

--- Source/Dialogs/AudioSettingsPanel.h
+++ Source/Dialogs/AudioSettingsPanel.h
@@ -1154,8 +1154,8 @@ public:
         dynamic_cast<DraggableNumber*>(latencyNumberBox.label.get())->setMinimum(64);
 
         auto* proc = dynamic_cast<PluginProcessor*>(processor);
-        auto& settingsTree = dynamic_cast<PluginProcessor*>(p)->settingsTree.getValueTree();
-
+        auto settingsTree = SettingsFile::getInstance()->getValueTree();
+        
         if (!settingsTree.hasProperty("NativeDialog")) {
             settingsTree.setProperty("NativeDialog", true, nullptr);
         }
