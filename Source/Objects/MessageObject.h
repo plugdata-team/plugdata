e89904998a3b4202e0bf0a2e74f822d7ec4729f4
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-01-28 03:39:02 +0100

    Fixed text scaling issue

--- Source/Objects/MessageObject.h
+++ Source/Objects/MessageObject.h
@@ -97,7 +97,7 @@ public:
         // Draw text
         if(!editor) {
             auto textArea = border.subtractedFrom(getLocalBounds().withTrimmedRight(5));
-            auto scale = getWidth() < 40 ? 0.8f : 1.0f;
+            auto scale = getWidth() < 50 ? 0.5f : 1.0f;
             
             PlugDataLook::drawFittedText(g, objectText, textArea, object->findColour(PlugDataColour::canvasTextColourId), numLines, scale);
         }
