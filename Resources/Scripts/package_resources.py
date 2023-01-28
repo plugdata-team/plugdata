b87e2c332ada72839a2a4dc9e9cd061a3f736c04
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-01-28 20:25:24 +0100

    Added [beat] abstraction to make it easier to receive DAW playhead

--- Resources/Scripts/package_resources.py
+++ Resources/Scripts/package_resources.py
@@ -87,6 +87,7 @@ globCopy("../../Libraries/pure-data/extra/**/*-help.pd", "./Abstractions")
 globCopy("../../Libraries/ELSE/Abstractions/*.pd", "./Abstractions/else")
 copyFile("../Patches/playhead.pd", "./Abstractions")
 copyFile("../Patches/param.pd", "./Abstractions")
+copyFile("../Patches/beat.pd", "./Abstractions")
 
 globMove("./Abstractions/*-help.pd", "./Documentation/5.reference")
 
