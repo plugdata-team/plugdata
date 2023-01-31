c8751adbf185f81ea548be982dc2753a550ddae9
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-01-31 16:45:43 +0100

    Fixed bug in [clone]

--- Source/Objects/ObjectBase.cpp
+++ Source/Objects/ObjectBase.cpp
@@ -277,30 +277,6 @@ void ObjectBase::paint(Graphics& g)
     g.drawRoundedRectangle(getLocalBounds().toFloat().reduced(0.5f), PlugDataLook::objectCornerRadius, 1.0f);
 }
 
-struct Lambda {
-    template<typename Tret, typename T>
-    static Tret lambda_ptr_exec(void* data)
-    {
-        return (Tret)(*(T*)fn<T>())(data);
-    }
-
-    template<typename Tret = void, typename Tfp = Tret (*)(void*), typename T>
-    static Tfp ptr(T& t)
-    {
-        fn<T>(&t);
-        return (Tfp)lambda_ptr_exec<Tret, T>;
-    }
-
-    template<typename T>
-    static void* fn(void* new_fn = nullptr)
-    {
-        static void* fn;
-        if (new_fn != nullptr)
-            fn = new_fn;
-        return fn;
-    }
-};
-
 void ObjectBase::initialiseParameters()
 {
     getLookAndFeel().setColour(Label::textWhenEditingColourId, object->findColour(Label::textWhenEditingColourId));
