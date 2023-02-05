23b400b81272ece5255d64a0a80f0fe949836454
alcomposer <alex.w.mitchell@gmail.com>
2023-02-06 01:24:38 +1030

    use FNV-1a hash for messages to simplify enum list

--- Source/Objects/NumberObject.h
+++ Source/Objects/NumberObject.h
@@ -153,7 +153,7 @@ public:
 
     void receiveObjectMessage(String const& symbol, std::vector<pd::Atom>& atoms) override
     {
-        switch (objectMessageMapped[symbol]) {
+        switch (hash(symbol.toUTF8())) {
         case objectMessage::msg_float:
         case objectMessage::msg_set: {
             value = std::clamp(atoms[0].getFloat(), static_cast<float>(min.getValue()), static_cast<float>(max.getValue()));
