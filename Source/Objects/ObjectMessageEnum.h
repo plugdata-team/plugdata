bb9f9371cbd4b04b368b10ec1491814a3bd06559
alcomposer <alex.w.mitchell@gmail.com>
2023-02-06 14:40:18 +1030

    use inline function, not template

--- Source/Objects/ObjectMessageEnum.h
+++ Source/Objects/ObjectMessageEnum.h
@@ -2,25 +2,32 @@
 
 #include <JuceHeader.h>
 
-//https://gist.github.com/ctmatthews/c1cbd1b2e68c29a88b236475a0b26cd0
-
 typedef uint32_t hash32;
 typedef uint8_t u8;
 #define EMPTY_HASH ((hash32)0x811c9dc5)
 
-template<typename T>
-constexpr hash32 hash(T str)
+/** 
+ *  FNV-1a hash function, compile and run time: https://gist.github.com/ctmatthews/c1cbd1b2e68c29a88b236475a0b26cd0
+ */ 
+constexpr hash32 hash(const char* str) 
 {
     hash32 result = EMPTY_HASH;
-    unsigned i = 0;
-    
-    while (str[i]) {
-        result ^= (hash32) str[i]; // NOTE: make this toupper(str[i]) or tolower(str[i]) if you want case-insensitive hashes
+
+    while (*str) {
+        result ^= (hash32) *str; // NOTE: make this toupper(*s) or tolower(*s) if you want case-insensitive hashes
         result *= (hash32) 0x01000193; // 32 bit magic FNV-1a prime
-        i++;
+        str++;
     }
 
     return result;
+}
+
+/**
+ * FNV-1a hash function, for juce::String, only at run time
+ */
+inline hash32 hash(const juce::String& str) 
+{
+    return hash(str.toUTF8().getAddress());
 }
 
 enum objectMessage {
