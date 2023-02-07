42973132518d0358819fca4e9932f4fe245ec513
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-02-07 21:04:22 +0100

    Resize on grid fixes

--- Source/Object.h
+++ Source/Object.h
@@ -106,6 +106,7 @@ public:
     std::unique_ptr<ObjectBoundsConstrainer> constrainer;
 
     Rectangle<int> originalBounds;
+    
 
 private:
     void initialise();
@@ -119,6 +120,8 @@ private:
     bool wasLockedOnMouseDown = false;
     bool indexShown = false;
     bool isHvccCompatible = true;
+        
+    bool wasResized = false;
 
     std::unique_ptr<TextEditor> newObjectEditor;
 
