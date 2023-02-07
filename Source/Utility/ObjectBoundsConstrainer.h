287206affb4bab1a4b167fffd0fe43d30608e0f5
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-02-07 23:59:02 +0100

    Fixed grid bug for object with fixed aspect ratio

--- Source/Utility/ObjectBoundsConstrainer.h
+++ Source/Utility/ObjectBoundsConstrainer.h
@@ -24,12 +24,13 @@ public:
         // we remove the margin from the resizing object
         BorderSize<int> border(Object::margin);
         border.subtractFrom(bounds);
-
+        
         // we also have to remove the margin from the old object, but don't alter the old object
         ComponentBoundsConstrainer::checkBounds(bounds, border.subtractedFrom(old), limits, isStretchingTop,
             isStretchingLeft,
             isStretchingBottom,
             isStretchingRight);
+
 
         // put back the margins
         border.addTo(bounds);
