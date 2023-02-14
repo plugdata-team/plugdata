e2247ce354cc3a92fa01e18f3c8d1cd352b9ec98
Nejrup <mikkel@nejrup.dk>
2023-02-14 14:07:11 +0100

    All resize zones snap to actual Grid

--- Source/ObjectGrid.cpp
+++ Source/ObjectGrid.cpp
@@ -200,14 +200,28 @@ Point<int> ObjectGrid::performResize(Object* toDrag, Point<int> dragOffset, Rect
 
     // Snap to Grid
     if (enableGrid != 1) {
-        Point<int> newPos = toDrag->originalBounds.reduced(Object::margin).getPosition() + dragOffset;
-        if (!isAlreadySnapped(true, true, dragOffset)) {
-            newPos.setX(roundToInt(newPos.getX() / gridSize + 1) * gridSize);
-            snappedPosition.x = newPos.x - toDrag->originalBounds.reduced(Object::margin).getX() - gridSize;
+        Point<int> newPosTopLeft = toDrag->originalBounds.reduced(Object::margin).getTopLeft() + dragOffset;
+        Point<int> newPosBotRight = toDrag->originalBounds.reduced(Object::margin).getBottomRight() + dragOffset;
+
+        if (toDrag->resizeZone.isDraggingLeftEdge() && !isAlreadySnapped(true, true, dragOffset)) {
+            std::cout << "Left" << std::endl;
+            newPosTopLeft.setX(roundToInt(newPosTopLeft.getX() / gridSize + 1) * gridSize);
+            snappedPosition.x = newPosTopLeft.x - toDrag->originalBounds.reduced(Object::margin).getX() - gridSize;
         }
-        if (!isAlreadySnapped(false, true, dragOffset)) {
-            newPos.setY(roundToInt(newPos.getY() / gridSize + 1) * gridSize);
-            snappedPosition.y = newPos.y - toDrag->originalBounds.reduced(Object::margin).getY() - gridSize;
+        if (toDrag->resizeZone.isDraggingTopEdge() && !isAlreadySnapped(false, true, dragOffset)) {
+            std::cout << "Top" << std::endl;
+            newPosTopLeft.setY(roundToInt(newPosTopLeft.getY() / gridSize + 1) * gridSize);
+            snappedPosition.y = newPosTopLeft.y - toDrag->originalBounds.reduced(Object::margin).getY() - gridSize;
+        }
+        if (toDrag->resizeZone.isDraggingRightEdge() && !isAlreadySnapped(true, true, dragOffset)) {
+            std::cout << "Rigth" << std::endl;
+            newPosBotRight.setX(roundToInt(newPosBotRight.getX() / gridSize + 1) * gridSize);
+            snappedPosition.x = newPosBotRight.x - toDrag->originalBounds.reduced(Object::margin).getRight() - gridSize + 1;
+        }
+        if (toDrag->resizeZone.isDraggingBottomEdge() && !isAlreadySnapped(false, true, dragOffset)) {
+            std::cout << "Bot" << std::endl;
+            newPosBotRight.setY(roundToInt(newPosBotRight.getY() / gridSize + 1) * gridSize);
+            snappedPosition.y = newPosBotRight.y - toDrag->originalBounds.reduced(Object::margin).getBottom() - gridSize + 1;
         }
         return snappedPosition;
     }
