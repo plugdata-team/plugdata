87f295848786a389af9e3bf994d29974f0f3a473
alcomposer <alex.w.mitchell@gmail.com>
2023-02-23 11:05:54 +1030

    cancel pan drag mode on mouseup

--- Source/Canvas.h
+++ Source/Canvas.h
@@ -89,6 +89,7 @@ public:
     SelectedItemSet<WeakReference<Component>>& getLassoSelection() override;
         
     bool checkPanDragMode();
+    bool setPanDragMode(bool shouldPan);
 
     void removeSelectedComponent(Component* component);
     void findLassoItemsInArea(Array<WeakReference<Component>>& itemsFound, Rectangle<int> const& area) override;
