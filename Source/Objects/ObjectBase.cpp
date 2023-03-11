92c86d12fbc66a0d0cbe393af8c4a3f35fed9c28
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-03-11 10:21:39 +0100

    fixed big bug

--- Source/Objects/ObjectBase.cpp
+++ Source/Objects/ObjectBase.cpp
@@ -536,6 +536,8 @@ void ObjectBase::receiveMessage(String const& symbol, int argc, t_atom* argv)
         case hash("height"):
             _this->object->updateBounds();
             break;
+        default:
+            _this->receiveObjectMessage(symbol, atoms);
         }
     });
 }
