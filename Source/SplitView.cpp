617d9edd37661f20a271d4ec7fa51123933afb1e
Nejrup <mikkel@nejrup.dk>
2023-03-15 21:57:10 +0100

    Fix tabs jumping when splitting

--- Source/SplitView.cpp
+++ Source/SplitView.cpp
@@ -309,12 +309,11 @@ void SplitView::splitCanvasView(Canvas* cnv, bool splitViewFocus)
     auto* editor = cnv->editor;
 
     auto* currentTabbar = cnv->getTabbar();
+    auto tabIndex = cnv->getTabIndex();
 
     currentTabbar->removeTab(cnv->getTabIndex());
 
-    if (currentTabbar->getCurrentTabIndex() < 0 && currentTabbar->getNumTabs() >= 0) {
-        currentTabbar->setCurrentTabIndex(0);
-    }
+    currentTabbar->setCurrentTabIndex(tabIndex - 1);
 
     cnv->recreateViewport();
 
