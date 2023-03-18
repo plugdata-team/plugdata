d6fdb55970aa46ac42d215f866ba3bb31b2f342b
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-03-18 20:24:52 +0100

    Fixed radiobox size problems

--- Source/Objects/RadioObject.h
+++ Source/Objects/RadioObject.h
@@ -59,7 +59,24 @@ public:
     {
         // radio stores it's height and width as a square to allow changing orientation via message: "orientation 0/1"
         b = isVertical ? b.withHeight(b.getWidth()) : b.withWidth(b.getHeight());
-        iemHelper.setPdBounds(b);
+        iemHelper.setPdBounds(b.withTrimmedRight(1).withTrimmedBottom(1));
+    }
+    
+    Rectangle<int> getPdBounds() override
+    {
+        pd->lockAudioThread();
+
+        int x = 0, y = 0, w = 0, h = 0;
+        libpd_get_object_bounds(cnv->patch.getPointer(), ptr, &x, &y, &w, &h);
+        
+        pd->unlockAudioThread();
+        
+        auto* radio = static_cast<t_radio*>(ptr);
+        
+        auto width = isVertical ? radio->x_gui.x_w : radio->x_gui.x_h * numItems;
+        auto height = isVertical ?  radio->x_gui.x_w * numItems : radio->x_gui.x_h;
+    
+        return {x, y, width + 1, height + 1};
     }
 
     void toggleObject(Point<int> position) override
@@ -135,35 +152,12 @@ public:
         return static_cast<t_radio*>(ptr)->x_on;
     }
 
-    Rectangle<int> getPdBounds() override
-    {
-        pd->lockAudioThread();
-
-        int x = 0, y = 0, w = 0, h = 0;
-        libpd_get_object_bounds(cnv->patch.getPointer(), ptr, &x, &y, &w, &h);
-        auto bounds = Rectangle<int>(x, y, w, h);
-
-        auto* radio = static_cast<t_radio*>(ptr);
-
-        if (isVertical) {
-            bounds.setSize(radio->x_gui.x_w, radio->x_gui.x_w * numItems);
-        } else {
-            bounds.setSize(radio->x_gui.x_h * numItems, radio->x_gui.x_h);
-        }
-
-        pd->unlockAudioThread();
-
-        return bounds;
-    }
-
     void paint(Graphics& g) override
     {
         g.setColour(iemHelper.getBackgroundColour());
         g.fillRoundedRectangle(getLocalBounds().toFloat().reduced(0.5f), Corners::objectCornerRadius);
 
-        int size = (isVertical ? getHeight() / numItems : getHeight());
-        // int minSize = 12;
-        // size = std::max(size, minSize);
+        float size = (isVertical ? static_cast<float>(getHeight()) / numItems : static_cast<float>(getWidth()) / numItems);
 
         g.setColour(object->findColour(PlugDataColour::objectOutlineColourId));
 
@@ -177,12 +171,11 @@ public:
 
         g.setColour(iemHelper.getForegroundColour());
 
-        int currentValue = selected;
-        int selectionX = isVertical ? 0 : currentValue * size;
-        int selectionY = isVertical ? currentValue * size : 0;
+        float selectionX = isVertical ? 0 : selected * size;
+        float selectionY = isVertical ? selected * size : 0;
 
-        auto selectionBounds = Rectangle<int>(selectionX, selectionY, size, size);
-        g.fillRoundedRectangle(selectionBounds.reduced(5).toFloat(), Corners::objectCornerRadius / 2.0f);
+        auto selectionBounds = Rectangle<float>(selectionX, selectionY, size, size);
+        g.fillRoundedRectangle(selectionBounds.reduced(5), Corners::objectCornerRadius / 2.0f);
     }
 
     void paintOverChildren(Graphics& g) override
