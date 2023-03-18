98b8400cfe7b67c0771ddf40ecaae781e357d313
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-03-18 17:06:57 +0100

    Fixed typo

--- Source/Objects/CommentObject.h
+++ Source/Objects/CommentObject.h
@@ -134,7 +134,7 @@ public:
 
         pd->unlockAudioThread();
 
-        return newBounds.withTrimmedBottom(4grap);
+        return newBounds.withTrimmedBottom(4);
     }
 
     bool checkBounds(Rectangle<int> oldBounds, Rectangle<int> newBounds, bool resizingOnLeft) override
