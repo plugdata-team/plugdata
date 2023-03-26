e03b5a53deaef4828a24d859ecdf850b418dddf0
Nejrup <mikkel@nejrup.dk>
2023-03-26 16:41:18 +0200

    disable minimise/maximise cleanup

--- Source/Standalone/PlugDataWindow.h
+++ Source/Standalone/PlugDataWindow.h
@@ -673,24 +673,28 @@ public:
         }
 
 #if JUCE_MAC
-        // Disable minimise/maximise in Plugin Mode if using native title bar
-        if (isUsingNativeTitleBar() && SettingsFile::getInstance()->getProperty<var>("plugin_mode") != var(false)) {
-            if (ComponentPeer* peer = getPeer())
-                OSUtils::HideTitlebarButtons(peer->getNativeHandle(), true, true, false);
-        } else if (isUsingNativeTitleBar()) {
-            if (ComponentPeer* peer = getPeer())
-                OSUtils::HideTitlebarButtons(peer->getNativeHandle(), false, false, false);
-        }
-
-        // Hide title bar buttons when fullscreen on MacOS
         if (!isUsingNativeTitleBar()) {
+            // Hide title bar buttons when fullscreen on MacOS
             bool visible = !isFullScreen();
             getCloseButton()->setVisible(visible);
             // & disable minimise and maximise in Plugin Mode
             visible = visible && SettingsFile::getInstance()->getProperty<var>("plugin_mode") == var(false);
             getMinimiseButton()->setVisible(visible);
             getMaximiseButton()->setVisible(visible);
+        } else if (SettingsFile::getInstance()->getProperty<var>("plugin_mode") != var(false)) {
+            // Disable minimise/maximise in Plugin Mode if using native title bar
+            if (ComponentPeer* peer = getPeer())
+                OSUtils::HideTitlebarButtons(peer->getNativeHandle(), true, true, false);
+        } else {
+            // Enable minimise/maximise
+            if (ComponentPeer* peer = getPeer())
+                OSUtils::HideTitlebarButtons(peer->getNativeHandle(), false, false, false);
         }
+#else
+        bool visible = SettingsFile::getInstance()->getProperty<var>("plugin_mode") == var(false);
+        // Disable minimise/maximise in Plugin Mode
+        getMinimiseButton()->setVisible(visible);
+        getMaximiseButton()->setVisible(visible);
 #endif
 
         getLookAndFeel().positionDocumentWindowButtons(*this, titleBarArea.getX(), titleBarArea.getY(), titleBarArea.getWidth(), titleBarArea.getHeight(), getMinimiseButton(), getMaximiseButton(), getCloseButton(), false);
