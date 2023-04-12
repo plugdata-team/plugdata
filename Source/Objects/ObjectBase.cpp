7ecc8ad9babc2d943cd6fdb21b464b3085c5898b
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-04-13 01:19:28 +0200

    Fixed potential crash

--- Source/Objects/ObjectBase.cpp
+++ Source/Objects/ObjectBase.cpp
@@ -512,7 +512,7 @@ void ObjectBase::receiveMessage(String const& symbol, int argc, t_atom* argv)
     auto messages = getAllMessages();
     if (std::find(messages.begin(), messages.end(), hash("anything")) != messages.end() || std::find(messages.begin(), messages.end(), sym) != messages.end()) {
         MessageManager::callAsync([_this = SafePointer(this), symbol, atoms]() mutable {
-            //if (_this)
+            if (_this)
                 _this->receiveObjectMessage(symbol, atoms);
         });
     }
