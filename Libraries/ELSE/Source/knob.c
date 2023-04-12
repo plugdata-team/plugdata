536cfd468a274496a1991a8b29a82ffd98197d75
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-04-13 01:26:21 +0200

    Fixed compilation

--- Libraries/ELSE/Source/knob.c
+++ Libraries/ELSE/Source/knob.c
@@ -802,5 +802,4 @@ void knob_setup(void){
     class_setwidget(knob_class, &knob_widgetbehavior);
     class_setsavefn(knob_class, knob_save);
     class_setpropertiesfn(knob_class, knob_properties);
-    #include "knob_dialog.h"
 }
