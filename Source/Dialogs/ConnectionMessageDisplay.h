28d196439edc7fdcb28cb753ae0b4c31df4da088
alcomposer <alex.w.mitchell@gmail.com>
2023-05-06 01:12:15 +0930

    fit connection info inside viewport

--- Source/Dialogs/ConnectionMessageDisplay.h
+++ Source/Dialogs/ConnectionMessageDisplay.h
@@ -4,6 +4,8 @@
 #include "Constants.h"
 #include "LookAndFeel.h"
 #include "../Connection.h"
+#include "../PluginEditor.h"
+#include "../CanvasViewport.h"
 
 class ConnectionMessageDisplay 
     : public Component
@@ -11,7 +13,7 @@ class ConnectionMessageDisplay
 public:
     ConnectionMessageDisplay()
     {
-        setSize(200,36);
+        setSize(36,36);
         startTimer(RepaintTimer, 1000/60);
         setVisible(false);
         // needed to stop the component from gaining mouse focus
@@ -28,7 +30,7 @@ public:
     {
         activeConnection = SafePointer<Connection>(connection);
         if (activeConnection.getComponent()) {
-            auto canvasConnectionPath = activeConnection->toDraw;
+            mousePosition = activeConnection->mouseHoverPos;
             startTimer(MouseHoverDelay, mouseDelay);
             stopTimer(MouseHoverExitDelay);
             updateTextString(true);
@@ -55,9 +57,9 @@ private:
             textString = StringArray("no message yet");
         }
 
-        auto fontStyle = haveMessage ? FontStyle::Bold : FontStyle::Regular;
-        auto textFont = Font(Fonts::getCurrentFont());
-        textFont.setSizeAndStyle(14, fontStyle, 1.0f, 0.0f);
+        auto fontStyle = haveMessage ? FontStyle::Semibold : FontStyle::Regular;
+        auto textFont = Font(haveMessage ? Fonts::getSemiBoldFont() : Fonts::getCurrentFont());
+        textFont.setSizeAndStyle(14, FontStyle::Regular, 1.0f, 0.0f);
         int stringWidth;
         int totalStringWidth = (8 * 2) + 4;
         String stringItem;
@@ -78,9 +80,20 @@ private:
             // calculate total needed width
             totalStringWidth += stringWidth + 4;
         }
+
+        Rectangle<int> proposedPosition;
         // only make the size wider, to fit changing values
-        if (totalStringWidth > getWidth() || isHoverEntered)
-            setSize(totalStringWidth, 36);
+        if (totalStringWidth > getWidth() || isHoverEntered) {
+            proposedPosition.setSize(totalStringWidth, 36);
+        } else 
+            proposedPosition.setSize(getWidth(), getHeight());
+
+        // make sure the proposed position is inside the viewport area
+        proposedPosition.setCentre(getParentComponent()->getLocalPoint(nullptr, mousePosition).translated(0, -(getHeight() * 0.5)));
+        auto activeCanvas = static_cast<PluginEditor*>(getParentComponent())->getCurrentCanvas();
+        auto viewArea = getParentComponent()->getLocalArea(activeCanvas, activeCanvas->viewport->getViewArea() / getValue<float>(activeCanvas->zoomScale));
+        auto constrainedPosition = proposedPosition.constrainedWithin(viewArea);
+        setBounds(constrainedPosition);
 
         repaint();
     }
@@ -95,8 +108,8 @@ private:
             }
         case MouseHoverDelay: {
             if (activeConnection.getComponent()) {
-                setCentrePosition(getParentComponent()->getLocalPoint(nullptr, activeConnection->mouseHoverPos).translated(0, -(getHeight() * 0.5)));
-                setVisible(activeConnection.getComponent());
+                updateTextString();
+                setVisible(true);
             }
             break;
         }
@@ -145,7 +158,9 @@ private:
             Fonts::drawStyledText(g, item.text, startPostionX, 0, item.width, getHeight(), findColour(PlugDataColour::panelTextColourId), item.fontStyle, 14, Justification::centredLeft);
             startPostionX += item.width + 4;
         }
-        //previousBounds = getBounds();
+
+        // used for cached background shadow
+        previousBounds = getBounds();
     }
 
     static inline bool isShowing = false;
@@ -160,6 +175,7 @@ private:
 
     Component::SafePointer<Connection> activeConnection;
     int mouseDelay = 500;
+    Point<int> mousePosition;
     enum TimerID {RepaintTimer, MouseHoverDelay, MouseHoverExitDelay};
 
     Point<float> circlePosition = { 8 + 4, 36 / 2 };
