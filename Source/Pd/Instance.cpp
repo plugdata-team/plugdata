57ae8aef1c7ad699511bd5dbbf1aadba8f9bdb79
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-06-16 18:08:13 +0200

    Compilation fix

--- Source/Pd/Instance.cpp
+++ Source/Pd/Instance.cpp
@@ -253,7 +253,7 @@ void Instance::initialisePd(String& pdlua_version)
         initialised = true;
     }
     
-    ofelia = std::make_unique<Ofelia>(m_instance);
+    ofelia = std::make_unique<Ofelia>(static_cast<t_pdinstance*>(m_instance));
     
     setThis();
 
