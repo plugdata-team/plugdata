bc79938dc8191cea5f3b2ce6e4be79dfb1be8b16
Timothy Schoen <timschoen123@gmail.com>
2023-07-31 12:41:39 +0200

    Revert changes to stackshadow

--- Source/Utility/StackShadow.h
+++ Source/Utility/StackShadow.h
@@ -19,7 +19,7 @@
 #endif
 
 #include <juce_audio_processors/juce_audio_processors.h>
-#include <juce_gui_basics/detail/juce_WindowingHelpers.h>
+
 class StackShadow {
 
     static inline unsigned short const stackblur_mul[255] = {
@@ -719,6 +719,12 @@ public:
     }
 };
 
+#if !JUCE_BSD
+namespace juce {
+bool isWindowOnCurrentVirtualDesktop(void* x);
+}
+#endif
+
 class StackDropShadower : private ComponentListener {
 public:
     /** Creates a DropShadower. */
@@ -1019,7 +1025,7 @@ private:
 #if JUCE_BSD
                     return false;
 #else
-                    return !detail::WindowingHelpers::isWindowOnCurrentVirtualDesktop(component->getWindowHandle());
+                    return !isWindowOnCurrentVirtualDesktop(component->getWindowHandle());
 #endif
                 }
 
