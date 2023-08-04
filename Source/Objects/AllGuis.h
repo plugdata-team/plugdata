76a0a820798fd27dbfd88c944d7b51abdbe707a8
Timothy Schoen <timschoen123@gmail.com>
2023-08-04 18:14:29 +0200

    Updating pd

--- Source/Objects/AllGuis.h
+++ Source/Objects/AllGuis.h
@@ -534,14 +534,14 @@ struct t_fake_fielddesc {
 // for template drawing
 struct t_fake_curve {
     t_object x_obj;
-    int x_flags; /* CLOSED, BEZ, NOMOUSERUN, NOMOUSEEDIT */
-    t_fake_fielddesc x_fillcolor;
-    t_fake_fielddesc x_outlinecolor;
-    t_fake_fielddesc x_width;
-    t_fake_fielddesc x_vis;
+    int x_flags;    /* CLOSED, BEZ, NOMOUSERUN, NOMOUSEEDIT */
+    t_fielddesc x_fillcolor;
+    t_fielddesc x_outlinecolor;
+    t_fielddesc x_width;
+    t_fielddesc x_vis;
     int x_npoints;
-    t_fake_fielddesc* x_vec;
-    t_canvas* x_canvas;
+    t_fielddesc *x_vec;
+    t_canvas *x_canvas;
 };
 
 // for template drawing
