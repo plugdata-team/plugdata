7506aff568546c12f046625e70d003f67a5daaad
Albert Graef <aggraef@gmail.com>
2023-08-04 05:16:52 +0200

    Revert "Fixed audio input and output ports not updating when enabling/disabling from settings"
    This reverts commit a8ae939aca4dc7d8cb5cc2a9565822957e24fe86.

--- Source/Dialogs/AudioSettingsPanel.h
+++ Source/Dialogs/AudioSettingsPanel.h
@@ -312,7 +312,6 @@ private:
                 inputProperties.add(new ChannelToggleProperty(channel, enabled, [this, idx](bool isEnabled) {
                     setup.useDefaultInputChannels = false;
                     setup.inputChannels.setBit(idx, isEnabled);
-                    updateConfig();
                 }));
                 idx++;
             }
@@ -341,7 +340,6 @@ private:
                 outputProperties.add(new ChannelToggleProperty(channel, enabled, [this, idx](bool isEnabled) {
                     setup.useDefaultOutputChannels = false;
                     setup.outputChannels.setBit(idx, isEnabled);
-                    updateConfig();
                 }));
                 idx++;
             }
