03e9e612569afc12fbf6ef7508d0f3db57f33af2
Timothy Schoen <timschoen123@gmail.com>
2023-08-13 16:16:29 +0200

    Fixed hvcc mode memory bug, added [array] to accepted object list

--- Source/Objects/SubpatchObject.h
+++ Source/Objects/SubpatchObject.h
@@ -142,7 +142,7 @@ public:
                 int size = 0;
                 libpd_get_object_text(object, &text, &size);
 
-                checkHvccCompatibility(patch, prefix + String::fromUTF8(text) + " -> ");
+                checkHvccCompatibility(patch, prefix + String::fromUTF8(text, size) + " -> ");
                 freebytes(static_cast<void*>(text), static_cast<size_t>(size) * sizeof(char));
 
             } else if (!Object::hvccObjects.contains(name)) {
