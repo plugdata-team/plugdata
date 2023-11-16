460e049d98bf9a369e6a85062f6b3ceb41ca19df
Timothy Schoen <timschoen123@gmail.com>
2023-11-16 01:47:39 +0100

    Fixes for suggestions component

--- Source/Pd/Library.cpp
+++ Source/Pd/Library.cpp
@@ -84,6 +84,9 @@ void Library::updateLibrary()
     // These can't be created by name in Pd, but plugdata allows it
     allObjects.add("graph");
     allObjects.add("garray");
+    
+    // This one isn't in there but should be
+    allObjects.add("list");
 
     sys_unlock();
 }
