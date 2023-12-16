23693c1bbf0f90fab9c3bc6134da067bbe611530
Timothy Schoen <timschoen123@gmail.com>
2023-12-16 01:32:03 +0100

    Fixed leak

--- Source/Utility/StackShadow.h
+++ Source/Utility/StackShadow.h
@@ -8,9 +8,15 @@ namespace melatonin
     class DropShadow;
 }
 
-struct StackShadow
+struct StackShadow : public juce::DeletedAtShutdown
 {
+    StackShadow();
+    
+    ~StackShadow();
+    
     static void renderDropShadow(juce::Graphics& g, juce::Path const& path, juce::Colour color, int const radius = 1, juce::Point<int> const offset = { 0, 0 }, int spread = 0);
     
-    static std::unique_ptr<melatonin::DropShadow> dropShadow;
+    melatonin::DropShadow* dropShadow;
+    
+    JUCE_DECLARE_SINGLETON(StackShadow, true)
 };
