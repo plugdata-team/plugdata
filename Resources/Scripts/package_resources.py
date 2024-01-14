4ee1615e0946c61e121e73dbc8adf7ecd972f0cc
Timothy Schoen <timschoen123@gmail.com>
2024-01-14 17:13:27 +0100

    Make sure helpfiles can find resources

--- Resources/Scripts/package_resources.py
+++ Resources/Scripts/package_resources.py
@@ -129,6 +129,7 @@ copyDir("../../Libraries/Gem/examples", "Documentation/14.gem/examples")
 copyDir("../../Libraries/Gem/doc", "Documentation/14.gem/examples/Documentation")
 globCopy("../../Libraries/Gem/abstractions/*.pd", "Abstractions/Gem/")
 globMove("Abstractions/Gem/*-help.pd", "Documentation/14.gem/")
+globCopy("../../Libraries/Gem/examples/data/*", "Extra/Gem/") # if we don't do this, helpfiles can't find resources for some reason
 makeDir("Extra/Gem") # user can put plugins in here
 
 changeWorkingDir("./..")
