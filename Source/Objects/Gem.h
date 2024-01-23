7eb242ccf861238305e58ed9e2d2f06889096eb4
Timothy Schoen <timschoen123@gmail.com>
2024-01-23 18:34:11 +0100

    Revert change for Windows

--- Source/Objects/Gem.h
+++ Source/Objects/Gem.h
@@ -232,7 +232,7 @@ int createGemWindow(WindowInfo& info, WindowHints& hints)
     gemJUCEWindow[window->instance].reset(window);
     info.window[window->instance] = window;
     
-    #if JUCE_LINUX || JUCE_WINDOWS
+    #if JUCE_LINUX
     // Make sure only audio thread has the context set as active
     window->openGLContext.executeOnGLThread([](OpenGLContext& context){
         // We get unpredictable behaviour if the context is active on multiple threads
