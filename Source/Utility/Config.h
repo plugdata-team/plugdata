01533cc7edeff8d10a267b7a099231c3ca04bf27
Timothy Schoen <timschoen123@gmail.com>
2024-02-16 21:20:05 +0100

    Incremented version suffix

--- Source/Utility/Config.h
+++ Source/Utility/Config.h
@@ -38,7 +38,7 @@ struct ProjectInfo {
 
     static inline File const appDataDir = File::getSpecialLocation(File::SpecialLocationType::userDocumentsDirectory).getChildFile("plugdata");
 
-    static inline String const versionSuffix = "-4";
+    static inline String const versionSuffix = "-5";
     static inline File const versionDataDir = appDataDir.getChildFile("Versions").getChildFile(ProjectInfo::versionString + versionSuffix);
 };
 
