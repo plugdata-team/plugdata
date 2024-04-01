caaaeeffb4c6e19a4f61643753945881c9997c0f
Timothy Schoen <timschoen123@gmail.com>
2024-04-02 00:24:02 +0200

    Change version suffix

--- Source/Utility/Config.h
+++ Source/Utility/Config.h
@@ -51,7 +51,7 @@ struct ProjectInfo {
 #else
     static inline File const appDataDir = File::getSpecialLocation(File::SpecialLocationType::userDocumentsDirectory).getChildFile("plugdata");
 #endif
-    static inline String const versionSuffix = "-8";
+    static inline String const versionSuffix = "-test";
     static inline File const versionDataDir = appDataDir.getChildFile("Versions").getChildFile(ProjectInfo::versionString + versionSuffix);
 };
 
