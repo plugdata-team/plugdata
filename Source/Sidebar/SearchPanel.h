0f0dcaab6714ab6934fb69f6a726d6f604f91b8d
alcomposer <alcomposer@users.noreply.github.com>
2024-05-09 15:50:20 +0930

    sync canvas when comment object hides editor also (if text has changed)

--- Source/Sidebar/SearchPanel.h
+++ Source/Sidebar/SearchPanel.h
@@ -321,7 +321,6 @@ public:
                                 }
                                 default:
                                     break;
-
                             }
                             break;
                         }
