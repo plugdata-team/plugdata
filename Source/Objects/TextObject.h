0a9f0095a727583129e7358955b460a2dd79ab6a
Timothy Schoen <timschoen123@gmail.com>
2024-07-04 00:48:44 +0200

    Only show hover in locked mode

--- Source/Objects/TextObject.h
+++ Source/Objects/TextObject.h
@@ -213,7 +213,7 @@ public:
             finalOutlineColour = convertColour(object->isSelected() ? Colours::red.brighter(1.5f) : Colours::red);
             finalBackgroundColour = nvgRGBAf(outlineColour.r, outlineColour.g, outlineColour.b, 0.2f);
         }
-        else if(getPatch() && isMouseOver())
+        else if(getPatch() && isMouseOver() && locked)
         {
             finalBackgroundColour = convertColour(getHoverBackgroundColour(backgroundColour));
         }
