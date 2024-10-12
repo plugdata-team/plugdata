eaafbdc92123531d79bff54aa22b714b80b94037
Timothy Schoen <timschoen123@gmail.com>
2024-10-12 18:00:43 +0200

    Use monospace font for code editor

--- Source/Dialogs/TextEditorDialog.h
+++ Source/Dialogs/TextEditorDialog.h
@@ -2238,7 +2238,7 @@ PlugDataTextEditor::PlugDataTextEditor()
     lastTransactionTime = Time::getApproximateMillisecondCounter();
     document.setSelections({ Selection() });
 
-    setFont(Font(Fonts::getCurrentFont().withHeight(15)));
+    setFont(Font(Fonts::getMonospaceFont().withHeight(15)));
 
     translateView(GUTTER_WIDTH, 0);
     setWantsKeyboardFocus(true);
