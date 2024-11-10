7c55ffe3a0b8cb9187ce33e4318fdfeeac15e8b0
alcomposer <alcomposer@users.noreply.github.com>
2024-11-10 17:03:29 +1030

    fix borderresizer not updating position when changed via canvas inspector

--- Source/Components/CanvasBorderResizer.h
+++ Source/Components/CanvasBorderResizer.h
@@ -5,7 +5,8 @@
  */
 
 class BorderResizer : public Component
-    , public NVGComponent {
+    , public NVGComponent
+    , public Value::Listener {
     ComponentDragger dragger;
     Canvas* cnv;
 
@@ -18,6 +19,15 @@ public:
     {
         setSize(8, 8);
         setRepaintsOnMouseActivity(true);
+
+        cnv->patchHeight.addListener(this);
+        cnv->patchWidth.addListener(this);
+    }
+
+    ~BorderResizer()
+    {
+        cnv->patchHeight.removeListener(this);
+        cnv->patchWidth.removeListener(this);
     }
 
     void mouseDown(MouseEvent const& e) override
@@ -32,8 +42,17 @@ public:
         if (cnv->showBorder) {
             auto constrainedPoint = getLocalPoint(cnv, Rectangle<int>(cnv->canvasOrigin.x + 11, cnv->canvasOrigin.y + 11, cnv->canvasOrigin.x, cnv->canvasOrigin.y).getConstrainedPoint(e.getEventRelativeTo(cnv).getPosition()));
             dragger.dragComponent(this, e.withNewPosition(constrainedPoint), nullptr);
+
+            cnv->patchHeight.removeListener(this);
+            cnv->patchWidth.removeListener(this);
             onDrag();
         }
+    }
+
+    void mouseUp(MouseEvent const& e) override
+    {
+        cnv->patchHeight.addListener(this);
+        cnv->patchWidth.addListener(this);
     }
 
     void render(NVGcontext* nvg) override
@@ -44,5 +63,15 @@ public:
         auto bounds = getLocalBounds().reduced(isMouseOver() ? 0 : 2);
         nvgDrawRoundedRect(nvg, bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight(), findNVGColour(PlugDataColour::canvasDotsColourId), findNVGColour(PlugDataColour::canvasDotsColourId), bounds.getWidth() / 2.0f);
         nvgRestore(nvg);
+    }
+
+    void valueChanged(Value& v) override
+    {
+        if (v.refersToSameSourceAs(cnv->patchHeight)) {
+            setCentrePosition(getBoundsInParent().getCentreX(), getValue<int>(cnv->patchHeight) + cnv->canvasOrigin.y);
+        }
+        else if (v.refersToSameSourceAs(cnv->patchWidth)) {
+            setCentrePosition(getValue<int>(cnv->patchWidth) + cnv->canvasOrigin.x, getBoundsInParent().getCentreY());
+        }
     }
 };
