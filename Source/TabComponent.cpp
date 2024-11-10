d0479b6e99a5af7a34bcaf26bccbf8396da2df2d
alcomposer <alcomposer@users.noreply.github.com>
2024-11-10 11:44:44 +1030

    Reset selection when changing active canvas

--- Source/TabComponent.cpp
+++ Source/TabComponent.cpp
@@ -867,8 +867,12 @@ void TabComponent::closeAllTabs(bool quitAfterComplete, Canvas* patchToExclude, 
 void TabComponent::setActiveSplit(Canvas* cnv)
 {
     if (cnv != splits[activeSplitIndex]) {
+        splits[activeSplitIndex]->deselectAll();
+
         activeSplitIndex = cnv == splits[1] ? 1 : 0;
         editor->nvgSurface.invalidateAll();
+
+        editor->sidebar->updateSearch();
     }
 }
 
