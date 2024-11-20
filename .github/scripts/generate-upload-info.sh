a1732440c0125df2e1ae8c320b0f197ba8cd0bb9
Timothy Schoen <timschoen123@gmail.com>
2024-11-20 22:33:11 +0100

    Workflow fix

--- .github/scripts/generate-upload-info.sh
+++ .github/scripts/generate-upload-info.sh
@@ -4,9 +4,12 @@ git config --global --add safe.directory /__w/plugdata/plugdata
 
 FILE=$1
 INFO_FILE=${FILE}.txt
-TIMESTAMP_FILE=${FILE}_timestamp.txt
 
 DATE=$(date -u +"%d-%m-%y %H:%M UTC")
-COMMIT_TIMESTAMP=$(git show -s --format=%ct HEAD)
+
+cat > $INFO_FILE <<END_FILE
+$DATE
+$GIT_HASH
+END_FILE
 
 exit 0
