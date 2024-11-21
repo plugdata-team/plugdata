5a351f8c6fac7174909a9c375613f14a31fb70c9
Timothy Schoen <timschoen123@gmail.com>
2024-11-21 01:11:51 +0100

    Last workflow fix

--- .github/scripts/generate-upload-info.sh
+++ .github/scripts/generate-upload-info.sh
@@ -4,7 +4,7 @@ git config --global --add safe.directory /__w/plugdata/plugdata
 
 FILE=$1
 INFO_FILE=${FILE}.txt
-GIT_HASH=$(git log -1 --format=%h)
+GIT_HASH=$(git log -1 --format=%H)
 DATE=$(date -u +"%d-%m-%y %H:%M UTC")
 
 cat > $INFO_FILE <<END_FILE
