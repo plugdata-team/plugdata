cf23b56dd83cdf0ddaa4090f41a57843e7765c13
Timothy Schoen <timschoen123@gmail.com>
2025-02-16 02:37:53 +0100

    Clean up, increment version suffix, small pd fix

--- Source/Objects/ObjectParameters.h
+++ Source/Objects/ObjectParameters.h
@@ -41,7 +41,7 @@ struct ObjectParameter {
     bool clip;
     double min, max;
 
-    ObjectParameter(String const& name, ParameterType const type, ParameterCategory const category, Value* valuePtr, StringArray const& options, var defaultValue, CustomPanelCreateFn createFn, InteractionFn interactionFn, bool clip, double min, double max)
+    ObjectParameter(String const& name, ParameterType const type, ParameterCategory const category, Value* valuePtr, StringArray const& options, var defaultValue, CustomPanelCreateFn createFn = nullptr, InteractionFn interactionFn = nullptr, bool clip = false, double min = 0.0, double max = 0.0)
         : name(name)
         , type(type)
         , category(category)
@@ -91,94 +91,89 @@ public:
 
     void addParamFloat(String const& pString, ParameterCategory const pCat, Value* pVal, var const& pDefault = var(), bool clip = false, double min = 0.0, double max = 0.0)
     {
-        objectParameters.add(makeParam(pString, tFloat, pCat, pVal, StringArray(), pDefault, nullptr, nullptr, clip, min, max));
+        objectParameters.add(ObjectParameter(pString, tFloat, pCat, pVal, StringArray(), pDefault, nullptr, nullptr, clip, min, max));
     }
 
     void addParamInt(String const& pString, ParameterCategory const pCat, Value* pVal, var const& pDefault = var(), bool clip = false, int min = 0, int max = 1<<30, InteractionFn onInteractionFn = nullptr)
     {
-        objectParameters.add(makeParam(pString, tInt, pCat, pVal, StringArray(), pDefault, nullptr, onInteractionFn, clip, min, max));
+        objectParameters.add(ObjectParameter(pString, tInt, pCat, pVal, StringArray(), pDefault, nullptr, onInteractionFn, clip, min, max));
     }
 
     void addParamBool(String const& pString, ParameterCategory const pCat, Value* pVal, StringArray const& pList, var const& pDefault = var())
     {
-        objectParameters.add(makeParam(pString, tBool, pCat, pVal, pList, pDefault));
+        objectParameters.add(ObjectParameter(pString, tBool, pCat, pVal, pList, pDefault));
     }
 
     void addParamString(String const& pString, ParameterCategory const pCat, Value* pVal, var const& pDefault = var())
     {
-        objectParameters.add(makeParam(pString, tString, pCat, pVal, StringArray(), pDefault));
+        objectParameters.add(ObjectParameter(pString, tString, pCat, pVal, StringArray(), pDefault));
     }
 
     void addParamColour(String const& pString, ParameterCategory const pCat, Value* pVal, var const& pDefault = var())
     {
-        objectParameters.add(makeParam(pString, tColour, pCat, pVal, StringArray(), pDefault));
+        objectParameters.add(ObjectParameter(pString, tColour, pCat, pVal, StringArray(), pDefault));
     }
 
     void addParamColourFG(Value* pVal)
     {
-        objectParameters.add(makeParam("Foreground", tColour, cAppearance, pVal, StringArray(), PlugDataColour::canvasTextColourId));
+        objectParameters.add(ObjectParameter("Foreground", tColour, cAppearance, pVal, StringArray(), PlugDataColour::canvasTextColourId));
     }
 
     void addParamColourBG(Value* pVal)
     {
-        objectParameters.add(makeParam("Background", tColour, cAppearance, pVal, StringArray(), PlugDataColour::guiObjectBackgroundColourId));
+        objectParameters.add(ObjectParameter("Background", tColour, cAppearance, pVal, StringArray(), PlugDataColour::guiObjectBackgroundColourId));
     }
 
     void addParamColourLabel(Value* pVal)
     {
-        objectParameters.add(makeParam("Color", tColour, cLabel, pVal, StringArray(), PlugDataColour::canvasTextColourId));
+        objectParameters.add(ObjectParameter("Color", tColour, cLabel, pVal, StringArray(), PlugDataColour::canvasTextColourId));
     }
 
     void addParamReceiveSymbol(Value* pVal)
     {
-        objectParameters.add(makeParam("Receive Symbol", tString, cGeneral, pVal, StringArray(), ""));
+        objectParameters.add(ObjectParameter("Receive Symbol", tString, cGeneral, pVal, StringArray(), ""));
     }
 
     void addParamSendSymbol(Value* pVal, String const& pDefault = "")
     {
-        objectParameters.add(makeParam("Send Symbol", tString, cGeneral, pVal, StringArray(), pDefault));
+        objectParameters.add(ObjectParameter("Send Symbol", tString, cGeneral, pVal, StringArray(), pDefault));
     }
 
     void addParamCombo(String const& pString, ParameterCategory const pCat, Value* pVal, StringArray const& pStringList, var const& pDefault = var())
     {
-        objectParameters.add(makeParam(pString, tCombo, pCat, pVal, pStringList, pDefault));
+        objectParameters.add(ObjectParameter(pString, tCombo, pCat, pVal, pStringList, pDefault));
     }
 
     void addParamRange(String const& pString, ParameterCategory const pCat, Value* pVal, VarArray const& pDefault = VarArray())
     {
-        objectParameters.add(makeParam(pString, tRangeFloat, pCat, pVal, StringArray(), pDefault));
+        objectParameters.add(ObjectParameter(pString, tRangeFloat, pCat, pVal, StringArray(), pDefault));
     }
 
     void addParamRangeInt(String const& pString, ParameterCategory const pCat, Value* pVal, VarArray const& pDefault = VarArray())
     {
-        objectParameters.add(makeParam(pString, tRangeInt, pCat, pVal, StringArray(), pDefault));
+        objectParameters.add(ObjectParameter(pString, tRangeInt, pCat, pVal, StringArray(), pDefault));
     }
 
     void addParamFont(String const& pString, ParameterCategory const pCat, Value* pVal, String const& pDefault = String())
     {
-        objectParameters.add(makeParam(pString, tFont, pCat, pVal, StringArray(), pDefault));
+        objectParameters.add(ObjectParameter(pString, tFont, pCat, pVal, StringArray(), pDefault));
     }
 
     void addParamPosition(Value* positionValue)
     {
-        objectParameters.add(makeParam("Position", tRangeInt, cDimensions, positionValue, StringArray(), var()));
+        objectParameters.add(ObjectParameter("Position", tRangeInt, cDimensions, positionValue, StringArray(), var()));
     }
 
     void addParamSize(Value* sizeValue, bool const singleDimension = false)
     {
-        objectParameters.add(makeParam("Size", singleDimension ? tInt : tRangeInt, cDimensions, sizeValue, StringArray(), var()));
+        objectParameters.add(ObjectParameter("Size", singleDimension ? tInt : tRangeInt, cDimensions, sizeValue, StringArray(), var()));
     }
 
     void addParamCustom(CustomPanelCreateFn customComponentFn)
     {
-        objectParameters.add(makeParam("", tCustom, cGeneral, nullptr, StringArray(), var(), customComponentFn));
+        objectParameters.add(ObjectParameter("", tCustom, cGeneral, nullptr, StringArray(), var(), customComponentFn));
     }
 
 private:
     HeapArray<ObjectParameter> objectParameters;
-
-    static ObjectParameter makeParam(String const& pString, ParameterType pType, ParameterCategory pCat, Value* pVal, StringArray const& pStringList, var const& pDefault, CustomPanelCreateFn customComponentFn = nullptr, InteractionFn onInteractionFn = nullptr, bool clip = false, double min = 0.0, double max = 0.0)
-    {
-        return { pString, pType, pCat, pVal, pStringList, pDefault, customComponentFn, onInteractionFn, clip, min, max};
-    }
 };
