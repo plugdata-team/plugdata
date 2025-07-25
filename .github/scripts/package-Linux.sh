42304ec3888bc413c7f9b26733407c63f8da458b
Timothy Schoen <timschoen123@gmail.com>
2025-07-25 22:36:28 +0200

    Also create xz archive for final Linux upload

--- .github/scripts/package-Linux.sh
+++ .github/scripts/package-Linux.sh
@@ -6,7 +6,7 @@ cp -r Plugins plugdata
 cp README.md plugdata/README.md
 cp LICENSE plugdata/LICENSE.txt
 
-# Create tar.gz with unique name for each distro
-tar -czvf $1 plugdata
+# Create tar.xz with unique name for each distro
+tar -cvf - plugdata | xz -9 > $1
 
 .github/scripts/generate-upload-info.sh $1
