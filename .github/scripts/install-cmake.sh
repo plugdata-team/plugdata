65b4e782bf116931d8bca958ecc304b4a00a43af
Timothy Schoen <timschoen123@gmail.com>
2026-02-13 20:52:10 +0100

    Update cmake installation script

--- .github/scripts/install-cmake.sh
+++ .github/scripts/install-cmake.sh
@@ -1,8 +1,8 @@
 case $(uname -m) in
-    x86_64)   curl -L https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2-linux-x86_64.sh --output ./do-install-cmake.sh ;;
-    arm)      curl -L https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2-linux-aarch64.sh --output ./do-install-cmake.sh ;;
-    arm64)    curl -L https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2-linux-aarch64.sh --output ./do-install-cmake.sh ;;
-    aarch64)  curl -L https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2-linux-aarch64.sh --output ./do-install-cmake.sh ;;
+    x86_64)   curl -L https://github.com/Kitware/CMake/releases/download/v3.31.0/cmake-3.31.0-linux-x86_64.sh --output ./do-install-cmake.sh ;;
+    arm)      curl -L https://github.com/Kitware/CMake/releases/download/v3.31.0/cmake-3.31.0-linux-aarch64.sh --output ./do-install-cmake.sh ;;
+    arm64)    curl -L https://github.com/Kitware/CMake/releases/download/v3.31.0/cmake-3.31.0-linux-aarch64.sh --output ./do-install-cmake.sh ;;
+    aarch64)  curl -L https://github.com/Kitware/CMake/releases/download/v3.31.0/cmake-3.31.0-linux-aarch64.sh --output ./do-install-cmake.sh ;;
 esac
 
 chmod +x do-install-cmake.sh
