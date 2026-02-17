7f14ae24de79e26da81bab8c43ccfeda24cf0bce
Timothy Schoen <timschoen123@gmail.com>
2026-02-17 13:42:33 +0100

    Reduce Linux download size

--- .github/scripts/package-Linux.sh
+++ .github/scripts/package-Linux.sh
@@ -6,6 +6,10 @@ cp -r Plugins plugdata
 cp README.md plugdata/README.md
 cp LICENSE plugdata/LICENSE.txt
 
+CANONICAL=$(find plugdata -name "libBinaryData.so" | head -1)
+if [ -n "$CANONICAL" ]; then
+    find plugdata -name "libBinaryData.so" ! -path "$CANONICAL" -exec ln -f "$CANONICAL" {} \;
+fi
 # Create tar.xz with unique name for each distro
 tar -cvf - plugdata | xz -9 > $1
 
