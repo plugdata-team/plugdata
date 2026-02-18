f1b89a92e23c51428c771430fd706380ae0170c5
Timothy Schoen <timschoen123@gmail.com>
2026-02-18 17:06:53 +0100

    Implement [button] hover

--- Source/Objects/ButtonObject.h
+++ Source/Objects/ButtonObject.h
@@ -151,6 +151,24 @@ public:
         }
     }
 
+    void mouseEnter(MouseEvent const& e) override
+    {
+        if (auto button = ptr.get<t_fake_button>()) {
+            outlet_float(button->x_obj.ob_outlet, state);
+            if(button->x_snd_hover->s_thing && !button->x_edit && button->x_snd_hover != gensym("empty") && button->x_snd_hover != pd->generateSymbol(""))
+                pd_float(button->x_snd_hover->s_thing, 1.0f);
+        }
+    }
+    
+    void mouseExit(MouseEvent const& e) override
+    {
+        if (auto button = ptr.get<t_fake_button>()) {
+            outlet_float(button->x_obj.ob_outlet, state);
+            if(button->x_snd_hover->s_thing && !button->x_edit && button->x_snd_hover != gensym("empty") && button->x_snd_hover != pd->generateSymbol(""))
+                pd_float(button->x_snd_hover->s_thing, 0.0f);
+        }
+    }
+    
     void mouseDown(MouseEvent const& e) override
     {
         if (!e.mods.isLeftButtonDown())
