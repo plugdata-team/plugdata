03469dc41dacfa86a555b667beacb561e9eac026
Timothy Schoen <timschoen123@gmail.com>
2026-02-23 11:47:20 +0100

    Fix for license panel

--- Source/Dialogs/AboutPanel.h
+++ Source/Dialogs/AboutPanel.h
@@ -182,7 +182,7 @@ class AboutPanel final : public Component {
     class LicensePanel final : public Component {
         TextEditor license;
 
-        static inline String const licenseText = "Copyright © Timothy Schoen\n"
+        static inline String const licenseText = String::fromUTF8("Copyright \xc2\xa9 Timothy Schoen\n"
         "\n"
         "This program is free software: you can redistribute it and/or modify\n"
         "it under the terms of the GNU Affero General Public License (AGPL-3.0) as published by the Free Software Foundation.\n"
@@ -191,7 +191,7 @@ class AboutPanel final : public Component {
         "\n"
         "This program is distributed in the hope that it will be useful,\n"
         "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
-        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.";
+        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.");
         
     public:
         LicensePanel() = default;
