fea7bad0f24ab7987173e279d0be0bc0c7cc4dc1
dreamer <dreamer@puikheid.nl>
2026-09-02 16:16:12 +0200

    use new ui_theme_set setting to prevent objects from getting their colors replaced

--- Source/Heavy/DPFExporter.h
+++ Source/Heavy/DPFExporter.h
@@ -219,23 +219,26 @@ public:
             metaDPF.getDynamicObject()->setProperty("enable_ui", 2);
             args.add("--gui");
 
+            var const metaTheme(new DynamicObject());
+            metaTheme.getDynamicObject()->setProperty("obj_corner_radius", getObjectCornerRadius(*this));
+            metaTheme.getDynamicObject()->setProperty("cnv_color", "#" + getThemeColours(*this).canvasBackgroundColour.toDisplayString(false));
+            metaTheme.getDynamicObject()->setProperty("cnv_txt_color", "#" + getThemeColours(*this).canvasTextColour.toDisplayString(false));
+
             if (useTheme) {
-                var const metaTheme(new DynamicObject());
-                metaTheme.getDynamicObject()->setProperty("obj_corner_radius", getObjectCornerRadius(*this));
-                metaTheme.getDynamicObject()->setProperty("cnv_txt_color", "#" + getThemeColours(*this).canvasTextColour.toDisplayString(false));
-                metaTheme.getDynamicObject()->setProperty("cnv_color", "#" + getThemeColours(*this).canvasBackgroundColour.toDisplayString(false));
+                metaDPF.getDynamicObject()->setProperty("ui_theme_set", 1);
+
                 metaTheme.getDynamicObject()->setProperty("io_color", "#" + getThemeColours(*this).guiObjectInternalOutlineColour.toDisplayString(false));
                 metaTheme.getDynamicObject()->setProperty("bg_color", "#" + getThemeColours(*this).guiObjectBackgroundColour.toDisplayString(false));
                 metaTheme.getDynamicObject()->setProperty("sel_color", "#" + getThemeColours(*this).objectSelectedOutlineColour.toDisplayString(false));
                 metaTheme.getDynamicObject()->setProperty("com_txt_color", "#" + getThemeColours(*this).commentTextColour.toDisplayString(false));
-                metaTheme.getDynamicObject()->setProperty("out_color", "#" + getThemeColours(*this).outlineColour.toDisplayString(false));
-
-                metaDPF.getDynamicObject()->setProperty("ui_theme", "default");
-
-                var const metaThemes(new DynamicObject());
-                metaThemes.getDynamicObject()->setProperty("default", metaTheme);
-                metaDPF.getDynamicObject()->setProperty("ui_themes", metaThemes);
+                metaTheme.getDynamicObject()->setProperty("out_color", "#" + getThemeColours(*this).outlineColour.toDisplayString(false));;
             }
+
+            metaDPF.getDynamicObject()->setProperty("ui_theme", "default");
+
+            var const metaThemes(new DynamicObject());
+            metaThemes.getDynamicObject()->setProperty("default", metaTheme);
+            metaDPF.getDynamicObject()->setProperty("ui_themes", metaThemes);
         }
 
         metaJson->setProperty("dpf", metaDPF);
