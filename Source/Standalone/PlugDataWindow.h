d6c78622f76e5378142553c2d3b5a41972b14320
Timothy Schoen <timschoen123@gmail.com>
2026-09-08 12:10:22 +0200

    Fix macOS window shadow bug

--- Source/Standalone/PlugDataWindow.h
+++ Source/Standalone/PlugDataWindow.h
@@ -400,10 +400,10 @@ public:
         }
 
         if (!nativeWindow) {
-#if JUCE_WINDOWS
-            setOpaque(true);
-#else
+#if JUCE_LINUX || JUCE_BSD
             setOpaque(false);
+#else
+            setOpaque(true);
 #endif
             setResizable(false, false);
             // we also need to set the constrainer of THIS window so it's set for the peer
